e04cd40f1d34111377f0b4d4999bb1fdc63db453
Julio Takayama <julio.takayama@bireme.org>
2013-02-14 10:48:38 -0200

    first upload to create the project

---
+++ twentytwelve_bireme/bireme_archives/admin/header.php
@@ -0,0 +1,34 @@
+<tr>
+	<th><label for="wp_bvs_tag_class">Logo URL</label></th>
+	<td>
+		<input id="header[logo]" name="header[logo]" type="text" class="regular-text code" value="<?php echo esc_html( stripslashes( $settings["header"]["logo"] ) ); ?>"></br>
+	</td>
+</tr>
+<tr>
+	<th><label for="header[banner]">Banner URL</label></th>
+	<td>
+		<input id="header[banner]" name="header[banner]" type="text" class="regular-text code" value="<?php echo esc_html( stripslashes( $settings["header"]["banner"] ) ); ?>"></br>
+	</td>
+</tr>
+<tr>
+	<th><label for="header[language]">Exibe Título no Banner?</label></th>
+	<td>
+		<input id="header[title_view]" name="header[title_view]" type="checkbox" class="" value="true" <?if($settings['header']['title_view'] == 'true') { echo "checked"; } ?> ></br>
+	</td>
+</tr>
+
+<tr>
+	<th><label for="header[language]">Exibe Idiomas disponíveis?</label></th>
+	<td>
+		<input id="header[language]" name="header[language]" type="checkbox" class="" value="true" <?if($settings['header']['language'] == 'true') { echo "checked"; } ?> ></br>
+	</td>
+</tr>
+<tr>
+	<th><label for="header-language-position">Posição da Barra de Idiomas</label></th>
+	<td>
+		<select class="header-language-position" id="language-position" name="header[language-position]">
+			<option <? if($settings['header']['language-position'] == "1") echo "selected='selected'"; ?> value="1">Acima</option>
+			<option <? if($settings['header']['language-position'] == "2") echo "selected='selected'"; ?> value="2">Abaixo</option>
+		</select>
+	</td>
+</tr>
---
+++ twentytwelve_bireme/bireme_archives/functions_bireme.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * 
+ * DEfinições específicas para BIREME
+ * 
+ */
+ /* Load up our theme options page and related code. */
+if ( is_admin() ) require_once( TEMPLATEPATH . '/bireme_archives/admin_settings.php' );
+
+$settings = get_option( "wp_bvs_theme_settings" );
+$total_columns = $settings['layout']['total'];
+$top_sidebar = $settings['layout']['top-sidebar'];
+$footer_sidebar = $settings['layout']['footer-sidebar'];
+
+// sidebars do template
+register_sidebar( array(
+	'name' => 'Header',
+	'id' => 'header',
+	'description' => '',
+	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	'after_widget' => '</aside>',
+	'before_title' => '<h3 class="widget-title">',
+	'after_title' => '</h3>',
+) );
+
+//SideBar Auxiliar Top só aparece se ativado
+if ($top_sidebar == true){
+	register_sidebar( array(
+		'name' => 'SideBar Auxiliar Top',
+		'id' => 'top_sidebar',
+		'description' => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+}
+
+// gerando as sidebars dinamicamente
+for($i=1; $i <= $total_columns; $i++) {
+
+	register_sidebar( array(
+		'name' => 'Coluna ' . $i,
+		'id' => 'column-' . $i,
+		'description' => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+}
+//SideBar Auxiliar Footer só aparece se ativado
+if ($footer_sidebar == true){
+	register_sidebar( array(
+		'name' => 'SideBar Auxiliar Footer',
+		'id' => 'footer_sidebar',
+		'description' => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+}
+
+register_sidebar( array(
+	'name' => 'Footer',
+	'id' => 'footer',
+	'description' => '',
+	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	'after_widget' => '</aside>',
+	'before_title' => '<h3 class="widget-title">',
+	'after_title' => '</h3>',
+) );
+
+
+
+?>
