c98d4ea1e0c8bca328d467d3eb725f9858ffd87b
Julio Takayama <julio.takayama@bireme.org>
2013-04-09 12:44:03 -0300

    including breadcrumb, hidding post navigation and fixing 2nd level layout

--- twentytwelve_bireme/single.php
+++ twentytwelve_bireme/single.php
@@ -8,29 +8,16 @@
  */
 
 get_header(); ?>
-
+	<div class="breadcrumb"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="home">Home</a> > <?php the_title(); ?></div>
 	<div id="primary" class="site-content">
-		<div id="content single1column" role="main">
-
+		<div id="content" class="single1column" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
-
 				<?php get_template_part( 'content', get_post_format() ); ?>
-
-				<nav class="nav-single">
-					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
-					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
-				</nav><!-- .nav-single -->
-
 				<?php comments_template( '', true ); ?>
-
 			<?php endwhile; // end of the loop. ?>
-
 		</div><!-- #content -->
 	<div class="single2column">
 		<?php dynamic_sidebar( 'level2' ); ?>
-
 	</div>
-
 	</div><!-- #primary -->
 <?php get_footer(); ?>
