91543925e2669d6c059558ced22592db10593bee
Renato Murasaki <renato.murasaki@gmail.com>
2013-09-27 09:59:39 -0300

    changing string for translating and textdomain

--- refnet/searchform.php
+++ refnet/searchform.php
@@ -8,9 +8,9 @@
  */
 ?>
         <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-                <label for="s" class="assistive-text"><?php _e( 'Search', 'twentyeleven' ); ?></label>
-                <input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'twentyeleven' ); ?>" />
+                <label for="s" class="assistive-text"><?php _e( 'Search form', 'refnet' ); ?></label>
+                <input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search by', 'refnet' ); ?>" />
                 <input type="hidden" class="field" name="l" id="l" value="<?php global $site_lang; echo $site_lang; ?>" />
-                <input type="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'twentyeleven' ); ?>" />
+                <input type="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'refnet' ); ?>" />
         </form>
 
