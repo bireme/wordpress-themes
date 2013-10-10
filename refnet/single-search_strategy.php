1e9792488a7fbdc760979e22201d778d4dee144e
Renato Murasaki <renato.murasaki@gmail.com>
2013-10-10 10:54:58 -0300

    Enhancement: Adapting call for bir_show_custom_field function

--- refnet/single-search_strategy.php
+++ refnet/single-search_strategy.php
@@ -18,90 +18,86 @@
 				<h4><?php the_title(); ?></h4>
 				<h5><?php _e('About the search','refnet');?></h5>
 				<dl>
-					<dt><?php echo __('Description of the search','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'description_of_the_search'); ?></dd>
-					<dt><?php echo __('Responsible','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'responsible'); ?></dd>
-					<dt><?php echo __('Deadlines','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'deadlines'); ?></dd>
+					<?php
+						$html4label = "<dt>label</dt>";
+						$html4custom_field = "<dd>custom_field</dd>";
+						echo bir_show_custom_field(get_the_ID(), 'description_of_the_search', __('Description of the search','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'responsible', __('Responsible','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'deadlines', __('Deadlines','refnet'), $html4label, $html4custom_field);
+
+					?>
 				</dl>
 				<h5><?php echo __('Search subject','refnet');?></h5>
 				<dl>
-					<dt><?php echo __('Main subject of the search','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'main_subject_of_the_search'); ?></dd>
-					<dt><?php echo __('Secondary subject of the search','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'secondary_subject_of_the_search'); ?></dd>
-					<dt><?php echo __('Other secondary subject of the search','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'other_secondary_subject_of_the_search'); ?></dd>
+					<?php
+						echo bir_show_custom_field(get_the_ID(), 'main_subject_of_the_search', __('Main subject of the search','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'secondary_subject_of_the_search', __('Secondary subject of the search','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'other_secondary_subject_of_the_search', __('Other secondary subject of the search','refnet'), $html4label, $html4custom_field);
+
+					?>
 				</dl>
 				<h5><?php echo __('Databases','refnet');?></h5>
 				<dl>
-					<dt><?php echo __('VHLs Databases','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'vhls_databases'); ?></dd>
-					<dt><?php echo __('Other VHLs Databases','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'other_vhls_databases'); ?></dd>
-					<dt><?php echo __('Other Databases','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'other_databases'); ?></dd>
-					<dt><?php echo __('More Other Databases','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'more_other_databases'); ?></dd>
+					<?php
+						echo bir_show_custom_field(get_the_ID(), 'vhls_databases', __('VHLs Databases','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'other_vhls_databases', __('Other VHLs Databases','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'other_databases', __('Other Databases','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'more_other_databases', __('More Other Databases','refnet'), $html4label, $html4custom_field);
+
+					?>
 				</dl>
 				<h5><?php echo __('General Search Filters','refnet');?></h5>
 				<dl>
-					<dt><?php echo __('Publication year','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'publication_year'); ?></dd>
-					<dt><?php echo __('Country or Region of publication','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'country_or_region_of_publication'); ?></dd>
-					<dt><?php echo __('Country or Region as subject','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'country_or_region_as_subject'); ?></dd>
-					<dt><?php echo __('Text language','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'text_language'); ?></dd>
-					<dt><?php echo __('Other Text language','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'other_text_language'); ?></dd>
-					<dt><?php echo __('Publication type','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'publication_type'); ?></dd>
-					<dt><?php echo __('Other Publication type','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'other_publication_type'); ?></dd>
-					<dt><?php echo __('Conditions: gender, age etc','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'conditions'); ?></dd>
-					<dt><?php echo __('Other Conditions','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'other_conditions'); ?></dd>
+					<?php
+						echo bir_show_custom_field(get_the_ID(), 'publication_year', __('Publication year','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'country_or_region_of_publication', __('Country or Region of publication','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'country_or_region_as_subject', __('Country or Region as subject','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'text_language', __('Text language','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'other_text_language', __('Other Text language','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'publication_type', __('Publication type','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'other_publication_type', __('Other Publication type','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'conditions', __('Conditions: gender, age etc','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'other_conditions', __('Other Conditions','refnet'), $html4label, $html4custom_field);
+
+					?>
 				</dl>
 				<h5><?php echo __('Search Strategy Scope','refnet');?></h5>
 				<dl>
-					<dt><?php echo __('VHL instance','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'vhl_instance'); ?></dd>
-					<dt><?php echo __('Type of search strategy','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'type_of_search_strategy'); ?></dd>
+					<?php
+						echo bir_show_custom_field(get_the_ID(), 'vhl_instance', __('VHL instance','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'type_of_search_strategy', __('Type of search strategy','refnet'), $html4label, $html4custom_field);
+
+					?>
 				</dl>
 				<h5><?php echo __('LILACS Strategy','refnet');?></h5>
 				<dl>
-					<dt><?php echo __('iAH Search Expression','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'lilacs_iah_search_expression'); ?></dd>
-					<dt><?php echo __('iAHx Search Expression','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'lilacs_iahx_search_expression'); ?></dd>
-					<dt><?php echo __('URL to Search Results','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'lilacs_url_to_search_results'); ?></dd>
+					<?php
+						echo bir_show_custom_field(get_the_ID(), 'lilacs_iah_search_expression', __('iAH Search Expression','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'lilacs_iahx_search_expression', __('iAHx Search Expression','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'lilacs_url_to_search_results', __('URL to Search Results','refnet'), $html4label, $html4custom_field);
+
+					?>
 				</dl>
 				<h5><?php echo __('MEDLINE Strategy','refnet');?></h5>
 				<dl>
-					<dt><?php echo __('iAH Search Expression','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'medline_iah_search_expression'); ?></dd>
-					<dt><?php echo __('iAHx Search Expression','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'medline_iahx_search_expression'); ?></dd>
-					<dt><?php echo __('URL to Search Results','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'medline_url_to_search_results'); ?></dd>
+					<?php
+						echo bir_show_custom_field(get_the_ID(), 'medline_iah_search_expression', __('iAH Search Expression','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'medline_iahx_search_expression', __('iAHx Search Expression','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'medline_url_to_search_results', __('URL to Search Results','refnet'), $html4label, $html4custom_field);
+
+					?>
 				</dl>
 				<h5><?php echo __('Cochrane Strategy','refnet');?></h5>
 				<dl>
-					<dt><?php echo __('iAH Search Expression','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'cochrane_iah_search_expression'); ?></dd>
-					<dt><?php echo __('iAHx Search Expression','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'cochrane_iahx_search_expression'); ?></dd>
-					<dt><?php echo __('URL to Search Results','refnet');?></dt>
-					<dd><?php echo bir_show_custom_field(get_the_ID(), 'cochrane_url_to_search_results'); ?></dd>
+					<?php
+						echo bir_show_custom_field(get_the_ID(), 'cochrane_iah_search_expression', __('iAH Search Expression','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'cochrane_iahx_search_expression', __('iAHx Search Expression','refnet'), $html4label, $html4custom_field);
+						echo bir_show_custom_field(get_the_ID(), 'cochrane_url_to_search_results', __('URL to Search Results','refnet'), $html4label, $html4custom_field);
+
+					?>
 				</dl>
 			</div>
-			<?php comments_template( '', true ); ?>
+			<?php comments_template(''); ?>
 			
 			
 			<?php while ( have_posts() ) : the_post(); ?>
