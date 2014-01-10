89bfe9b611732b22e0a5b6755cbfaee3dd7db1c6
Renato Murasaki <renato.murasaki@gmail.com>
2014-01-10 16:42:27 -0200

    Changes on the title, subtitle and their translation to portuguese and spanish. Ticket #33

--- refnet/header.php
+++ refnet/header.php
@@ -82,9 +82,9 @@
 	            		<?php if ($title == true) {	?>
 		   		<div class="site_name">
 					<h1>
-						<a title="<?php  _e('Topic-specific queries', 'refnet'); ?>" href="<?php global $site_lang; echo $bannerLink . '?l=' . $site_lang;?>"><span><?php  _e('Topic-specific queries', 'refnet'); ?></span></a>
+						<a title="<?php  _e('Repository of search strategies on the VHL', 'refnet'); ?>" href="<?php global $site_lang; echo $bannerLink . '?l=' . $site_lang;?>"><span><?php  _e('Repository of search strategies on the VHL', 'refnet'); ?></span></a>
 						<br/>
-						<small><?php _e('Repository of search strategies on the VHL','refnet') ?></small>
+						<small><?php _e('Topic-specific queries','refnet') ?></small>
 					</h1>
 		        	</div>
 				<?php } ?>
