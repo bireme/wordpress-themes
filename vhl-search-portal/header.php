5b9c1f53c0d6a6e8226de56f366df56d0aa29642
juliotak <takayama@paho.org>
2014-04-23 10:57:43 -0300

    fixing accessibility issues

--- vhl-search-portal/header.php
+++ vhl-search-portal/header.php
@@ -8,24 +8,22 @@ $mlf_options = get_option('mlf_config');
 $current_language = get_bloginfo('language');
 $site_lang = substr($current_language, 0,2);
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo ( $current_language ); ?>">
 
-<head profile="http://gmpg.org/xfn/11">
-		
-	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-
+<head>
 	<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-	
+
 	<noscript><?php _e('Enable JavaScript');?></noscript>
+	
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<meta name="language" content="<?php echo ( $site_lang ); ?>" />
+	<meta name="viewport" content="width=device-width, user-scalable=no" />
 	
 	<link rel="stylesheet" media="screen" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 	<link rel="stylesheet" media="screen" href="<?php bloginfo( 'stylesheet_directory' ); ?>/skin.css" />
-
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-	<link href="/favicon.ico" rel="shortcut icon">
-
+	<link href="/favicon.ico" rel="shortcut icon" />
 	<?php wp_head(); ?>
 </head>
 
