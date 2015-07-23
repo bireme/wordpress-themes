1cf250263888db2f042e65d39e6c3b3e6376cef3
Wilson Moura <wilsonsmoura@gmail.com>
2015-07-23 17:20:10 -0300

    Include SOFs module in admin area.

--- bvs-controlecancer/functions.php
+++ bvs-controlecancer/functions.php
@@ -3,7 +3,18 @@
  * @package WordPress
  * @subpackage Classic_Theme
  */
+// LANGUAGE 
+global $site_lang;
+global $current_language;
+
+$site_lang = get_locale($locale);
 $current_language = strtolower(get_bloginfo('language'));
+
+$wpdecs_array_locale = array(
+    'pt_BR' => 'pt',
+    'en_US' => 'en',
+    'es_ES' => 'es'
+);
 
 automatic_feed_links();
 
@@ -54,4 +65,125 @@ if (function_exists('add_theme_support')) {
     add_image_size('small_highlight', 60, 40, true);
     add_image_size('icon', 16, 16, true);
 }
+
+add_filter( 'manage_edit-aps_columns', 'edit_aps_columns' ) ;
+function edit_aps_columns( $columns ) {
+
+    $columns = array(
+        'cb' => '<input type="checkbox" />',
+        'title' => __( 'SOF' ),
+        'ciap2' => __( 'CIAP2' ),
+        'tipo-de-profissional' => __( 'Tipo de Profissional' ),
+        'date' => __( 'Date' )
+    );
+
+    return $columns;
+}
+
+add_action( 'manage_aps_posts_custom_column', 'manage_aps_columns', 10, 2 );
+function manage_aps_columns( $column, $post_id ) {
+    global $post;
+
+    switch( $column ) {
+
+        /* If displaying the 'ciap2' column. */
+        case 'ciap2' :
+
+            /* Get the genres for the post. */
+            $terms = get_the_terms( $post_id, 'ciap2' );
+
+            /* If terms were found. */
+            if ( !empty( $terms ) ) {
+
+                $out = array();
+
+                /* Loop through each term, linking to the 'edit posts' page for the specific term. */
+                foreach ( $terms as $term ) {
+                    $out[] = sprintf( '<a href="%s">%s</a>',
+                        esc_url( add_query_arg( array( 'post_type' => $post->post_type, 'ciap2' => $term->slug ), 'edit.php' ) ),
+                        esc_html( sanitize_term_field( 'name', $term->name, $term->term_id, 'ciap2', 'display' ) )
+                    );
+                }
+
+                /* Join the terms, separating them with a comma. */
+                echo join( ', ', $out );
+            }
+
+            /* If no terms were found, output a default message. */
+            else {
+                _e( 'Nenhum' );
+            }
+
+            break;
+
+        /* Just break out of the switch statement for everything else. */
+        default :
+            break;
+    }
+}
+
+//add_filter( 'manage_edit-aps_sortable_columns', 'aps_sortable_columns' );
+function aps_sortable_columns( $columns ) {
+
+    $columns['ciap2'] = 'ciap2';
+
+    return $columns;
+}
+
+/* Only run our customization on the 'edit.php' page in the admin. */
+//add_action( 'load-edit.php', 'edit_aps_load' );
+
+function edit_aps_load() {
+    add_filter( 'request', 'sort_bvsaps' );
+}
+
+/* Sorts the SOFs. */
+function sort_bvsaps( $vars ) {
+
+    /* Check if we're viewing the 'aps' post type. */
+    if ( isset( $vars['post_type'] ) && 'aps' == $vars['post_type'] ) {
+
+        /* Check if 'orderby' is set to 'ciap2'. */
+        if ( isset( $vars['orderby'] ) && 'ciap2' == $vars['orderby'] ) {
+
+            /* Merge the query vars with our custom variables. */
+            $vars = array_merge(
+                $vars,
+                array(
+                    'meta_key' => 'ciap2',
+                    'orderby' => 'meta_value_num'
+                )
+            );
+        }
+    }
+
+    return $vars;
+}
+
+function aps_cp_filter() {
+    global $typenow;
+
+    // select the custom taxonomy
+    $taxonomies = array('ciap2');
+
+    // select the type of custom post
+    if( $typenow == 'aps' ){
+
+        foreach ($taxonomies as $tax_slug) {
+            $tax_obj = get_taxonomy($tax_slug);
+            $tax_name = $tax_obj->labels->name;
+            $terms = get_terms($tax_slug);
+            if(count($terms) > 0) {
+                echo "<select name='$tax_slug' id='$tax_slug' class='postform'>";
+                echo "<option value=''>Show All $tax_name</option>";
+                foreach ($terms as $term) {
+                    echo '<option value='. $term->slug, $_GET[$tax_slug] == $term->slug ? ' selected="selected"' : '','>' . $term->name .' (' . $term->count .')</option>';
+                }
+                echo "</select>";
+            }
+        }
+    }
+}
+add_action( 'restrict_manage_posts', 'aps_cp_filter' );
+
 ?>
---
+++ bvs-controlecancer/single-aps.php
@@ -0,0 +1,125 @@
+<?php 
+
+//load_theme_textdomain('bvsaps', get_stylesheet_directory() . '/languages');
+
+global $site_lang;
+global $wp_query; 
+$post_type = ($wp_query->query_vars['post_type'] == 'aps') ? __('SOF', 'bvsaps') : __('PEARL', 'bvsaps');
+
+get_header(); ?>
+
+<div class="single aps">
+	
+	<div class="container">
+			
+		<?php while(have_posts()): the_post(); ?>
+			
+			<div class="item">
+
+				<h3 class="post-type"><?php _e("SOF", 'bvsaps'); ?></h3>
+				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				
+				
+				
+				<div class="dados">
+					<?php // Estes campos são os mesmos. Coloquei os dois pois há ambientes em que a taxonomy foi criada como area-tematica e outros
+					// em que a taxonomy foi criada com categoria-da-evidencia; ?>	
+					<?php if(taxonomy_exists('area-tematica')): ?>
+						<div class="category"><?php the_terms(get_the_ID(), 'area-tematica'); ?></div>
+					<?php endif; ?>
+
+					<?php if(taxonomy_exists('categoria-da-evidencia')): ?>
+						<div class="category"><?php the_terms(get_the_ID(), 'categoria-da-evidencia'); ?></div>
+					<?php endif; ?>
+					
+					<div class="thumb">
+						<?php if(taxonomy_exists('area-tematica')): ?>
+							<?php foreach (get_the_terms(get_the_ID(), 'area-tematica') as $cat): ?>
+								<img src="<?php echo z_taxonomy_image_url($cat->term_id, 'single-thumb-square'); ?>" />
+							<?php break; endforeach; ?>
+						<?php endif; ?>
+
+						<?php if(taxonomy_exists('categoria-da-evidencia')): ?>
+							<?php foreach (get_the_terms(get_the_ID(), 'categoria-da-evidencia') as $cat): ?>
+								<img src="<?php echo z_taxonomy_image_url($cat->term_id, 'single-thumb-square'); ?>" />
+							<?php break; endforeach; ?>
+						<?php endif; ?>
+					</div>
+					<p>
+						<?php $term_content = get_the_terms(get_the_ID(), 'tipo-de-profissional'); if(!empty($term_content)): ?>
+							<b><?php _e('Profissional Solicitante', 'bvsaps'); ?>: </b><?php the_terms(get_the_ID(), 'tipo-de-profissional'); ?><br>
+						<?php endif; ?>
+						
+						<?php $term_content = get_the_terms(get_the_ID(), 'ciap2'); if(!empty($term_content)): ?>
+							<b><?php _e('CIAP2', 'bvsaps'); ?>: </b><?php the_terms(get_the_ID(), 'ciap2'); ?><br>
+						<?php endif; ?>
+						
+						<?php if(function_exists('get_the_wpdecs_terms')): $wpdecs_terms = get_the_wpdecs_terms(); ?>
+							<b><?php _e('DeCS/MeSH', 'bvsaps'); ?>: </b>
+							<?php $count = 0; foreach($wpdecs_terms as $term): ?>
+
+								<?php
+									$link = false;
+									$term_obj = get_term_by('name', $term['lang'][$wpdecs_array_locale[$site_lang]], 'decs');
+									if($term_obj != false) {
+										$link = get_term_link($term_obj, 'decs');
+									}
+								?>
+								
+								<?php if($count > 0) print ","; ?>
+
+								<!-- caso achar o link, printa o começo do <a> -->
+								<?php if($link != false): ?>
+									<a href="<?= $link; ?>" title="<?= $term['lang'][$wpdecs_array_locale[$site_lang]]; ?>">
+								<?php endif; ?>
+
+								<?= $term['lang'][$wpdecs_array_locale[$site_lang]]; ?>
+								<!--
+								<?php $quals = array(); foreach($term['qualifier'] as $qual) 
+									$quals[] = $qual['name'];
+								?>
+								-->
+								<?php if(isset($term['qualifier']) and !empty($term['qualifier'])): ?>
+									<!-- (<?php _e("Qualificadores", 'bvsaps'); ?>: <?php print join($quals, ", "); ?>) -->
+									<!-- (<?php print join($quals, ", "); ?>) -->
+									(<?php print join($term["qualifier"], ", "); ?>)
+								<?php endif; ?>
+
+								<!-- caso achar o link, printa o fim do <a> -->
+								<?php if($link != false): ?>
+									</a>
+								<?php endif; ?>
+
+							<?php $count++; endforeach; ?>
+							<br>
+						<?php endif; ?>
+						
+						<?php $term_content = get_the_terms(get_the_ID(), 'grau-da-evidencia'); if(!empty($term_content)): ?>
+							<b><?php _e('Grau da Evidência', 'bvsaps'); ?>: </b><?php the_terms(get_the_ID(), 'grau-da-evidencia'); ?><br>
+						<?php endif; ?>
+					</p>
+				</div>
+
+				
+				<div class="content">
+					<p><?php the_content(); ?></p>
+				</div>
+
+				<div class="notices">
+					<p><?php the_field('observacoes'); ?></p>
+				</div>
+
+				<b><?php _e('Bibliografia Selecionada', 'bvsaps'); ?></b><br>
+				<p><?php the_field('bibliografia_selecionada'); ?></p>
+
+				<div class="clear"></div>
+			</div>
+
+		<?php endwhile; ?>
+	
+		<?php //kriesi_pagination(); ?>
+	</div>
+	
+</div>
+
+<?php get_footer(); ?>
