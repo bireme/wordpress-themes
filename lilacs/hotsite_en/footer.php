2c30b187cf092a3e35a475ea3d19ba75b8c47788
marcoslmori <gigio0107@gmail.com>
2016-08-12 14:54:08 -0300

    address change

--- lilacs/hotsite_en/footer.php
+++ lilacs/hotsite_en/footer.php
@@ -4,7 +4,7 @@
 				<div class="footerLogo">
 				</div>
 				<div class="institution">
-					<b>BIREME - PAHO - WHO</b><br>Latin American and Caribbean Center on Health Sciencies Information<br>Rua Botucatu, 862 - cep: 04023-901 - São Paulo/SP - Brasil<br>Tel: (55 11) 5576-9800 - Fax: (55 11) 5575-8868				</div>
+					<b>BIREME - PAHO - WHO</b><br>Latin American and Caribbean Center on Health Sciencies Information<br>Knowledge Management, Bioethics and Research Area - KBR<br>Rua Vergueiro, 1759 | cep: 04101-000 | São Paulo - SP | <br>Tel: (55 11) 5576-9800 | Fax: (55 11) 5575-8868<br><a href="http://www.paho.org/bireme/">http://www.paho.org/bireme/</a></div>
 			</div>
 			<?php wp_footer(); ?>
 			<div class="spacer"></div>
