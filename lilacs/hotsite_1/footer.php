3166adeb5a5cd7586e93ecfa9fda0f0d901f5a8a
marcoslmori <gigio0107@gmail.com>
2016-08-12 14:38:00 -0300

    adress change

--- lilacs/hotsite_1/footer.php
+++ lilacs/hotsite_1/footer.php
@@ -4,7 +4,7 @@
 				<div class="footerLogo">
 				</div>
 				<div class="institution">
-					<b>BIREME - OPAS - OMS</b><br>Centro Latino-Americano e do Caribe de Informação em Ciências da Saúde<br>Rua Botucatu, 862 - cep: 04023-901 - São Paulo/SP - Brasil<br>Tel: (55 11) 5576-9800 - Fax: (55 11) 5575-8868
+					<b>BIREME - OPAS - OMS</b><br>Centro Latino-Americano e do Caribe de Informação em Ciências da Saúde<br>Área de Gestão do Conhecimento, Bioética e Pesquisa - KBR<br>Rua Vergueiro, 1759 | cep: 04101-000 | São Paulo - SP  - Brasil<br>Tel: (55 11) 5576-9800 | Fax: (55 11) 5575-8868<br><a href="http://www.paho.org/bireme/">http://www.paho.org/bireme/</a>
 				</div>
 			</div>
 			<?php wp_footer(); ?>
