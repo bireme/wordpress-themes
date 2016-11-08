d5aff435571e113c3b3473280844363a71ac5312
Wilson Moura <wilsonsmoura@gmail.com>
2016-11-08 11:29:18 -0200

    Add PMM theme

---
+++ pmm/functions.php
@@ -0,0 +1,13 @@
+<?php
+function theme_enqueue_styles() {
+
+    $parent_style = 'parent-style';
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style )
+    );
+
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
