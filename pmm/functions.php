6db933ee24e3d13480c17c512d2acf49146f8ae3
Wilson Moura <wilsonsmoura@gmail.com>
2016-11-23 12:44:28 -0200

    Include breadcrumb in page template

--- pmm/functions.php
+++ pmm/functions.php
@@ -11,3 +11,23 @@ function theme_enqueue_styles() {
 
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+function home_breadcrumb_title($title, $type, $id)
+{
+    if( in_array( 'home', $type ) && !$id )
+        $title = 'HOME';
+    return $title;
+}
+add_filter('bcn_breadcrumb_title', 'home_breadcrumb_title', 3, 10);
+
+function home_breadcrumb_url($url, $type, $id)
+{
+    if( defined( 'POLYLANG_VERSION' ) && in_array( 'home', $type ) && !$id ) {
+    	$dl = pll_default_language(); // default language
+    	$cl = pll_current_language(); // current language
+    	if ( $dl != $cl )
+        	$url = pll_home_url();
+    }
+    return $url;
+}
+add_filter('bcn_breadcrumb_url', 'home_breadcrumb_url', 3, 10);
---
+++ pmm/page.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+    <div class="breadcrumb" typeof="BreadcrumbList" vocab="http://schema.org/">
+        <?php if(function_exists('bcn_display')) bcn_display(); ?>
+    </div>
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', 'page' ); ?>
+				<?php comments_template( '', true ); ?>
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
