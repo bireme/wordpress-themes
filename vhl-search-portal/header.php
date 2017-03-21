98dadb371ac5c6429149223d91ccffbcca73f090
Wilson Moura <wilsonsmoura@gmail.com>
2017-03-21 15:19:13 -0300

    Minor fixes

--- vhl-search-portal/header.php
+++ vhl-search-portal/header.php
@@ -27,7 +27,7 @@ $suffix = ( !defined( 'POLYLANG_VERSION' ) ) ? '_' . $current_language : '';
 	<?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
     <div class="container">
         <div class="bar">
 			<div class="barInner">
