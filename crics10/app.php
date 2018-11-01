c33e190821518391bbfc74b0bb4d0ce4de7e88ca
Wilson Moura <wilsonsmoura@gmail.com>
2018-11-01 14:50:18 -0300

    Update app.php

--- crics10/app.php
+++ crics10/app.php
@@ -1,27 +1,27 @@
 <?php get_header('app'); ?>
 
-	<div id="wpsp-container">
+    <div id="wpsp-container">
 
-		<!-- === Title === -->
+        <!-- === Title === -->
 
-		<h1 id="wpsp-title">CRICS10</h1>
+        <h1 id="wpsp-title">CRICS10</h1>
 
-		<!-- === Text === -->
+        <!-- === Text === -->
 
-		<div id="wpsp-text">
-			<p>Please choose a language</p>
-			<p>Por favor, escolha um idioma</p>
-			<p>Por favor, elija un idioma</p>
-		</div>
-
-		<!-- === Continue Button === -->
-
-		<div class="languages">
-			<a class="wpsp-continue" href="http://www.teste.crics10.org/pt/app-pt/">Português</a>
-        	<a class="wpsp-continue" href="http://www.teste.crics10.org/app-es/">Español</a>
-        	<a class="wpsp-continue" href="http://www.teste.crics10.org/en/app-en/">English</a>
+        <div id="wpsp-text">
+            <p>Please choose a language</p>
+            <p>Por favor, escolha um idioma</p>
+            <p>Por favor, elija un idioma</p>
         </div>
 
-	</div> <!-- === END #wpsp-container === -->
+        <!-- === Continue Button === -->
+
+        <div class="languages">
+            <a class="wpsp-continue" href="http://www.crics10.org/app-pt/">Português</a>
+            <a class="wpsp-continue" href="http://www.crics10.org/app-es/">Español</a>
+            <a class="wpsp-continue" href="http://www.crics10.org/app-en/">English</a>
+        </div>
+
+    </div> <!-- === END #wpsp-container === -->
 
 <?php get_footer('app'); ?>
