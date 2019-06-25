67d323fbde9ee474264942cf1dd3fc1a793ac858
da Silva <dasilvmar@paho.org>
2019-06-25 10:48:20 -0300

    Banner Rresponsivo
    Modificação para que o banner carregue duas imagens

--- globalindexmedicus/includes/banner.php
+++ globalindexmedicus/includes/banner.php
@@ -2,31 +2,30 @@
 <div id="banners" class="carousel slide" data-ride="carousel">
 	<div class="carousel-inner">
 		<?php 
-		$banners = new WP_Query(array(
-			'post_type' => 'banners',
-				// 'orderby' => 'title',
-				// 'order'   => 'ASC'
-		));
-			$i = 0;
-		while($banners->have_posts()) : $banners->the_post();
-			$itens  = get_field('banners');
-			while( have_rows('banners') ): the_row(); 
-				// vars
-				$imagem = get_sub_field('imagem');
-				$texto = get_sub_field('texto');
-				$link = get_sub_field('link');
-				
-				?>
-				<div class="carousel-item <?php echo ($i == 0) ? 'active' : ''; ?> ">
-					<img src="<?php echo $imagem['url']; ?>" alt="<?php echo $imagem['alt'] ?>" />
-					<div class="carousel-caption text-left">
-						<h5><?php echo $texto; ?></h5>
-						<div class="clearfix"></div>
-						<a href="<?php echo $link; ?>"  class="bannerLink" ><?php pll_e('Saiba Mais'); ?> [+]</a>
-					</div>
+	$banners = new WP_Query([
+		'post_type' => 'banners',
+	]);
+	$i = 0;
+	while($banners->have_posts()) : $banners->the_post();
+		$itens = get_field('cadastrar_banners');
+		while( have_rows('cadastrar_banners') ): the_row(); 
+			$fotoMobile = get_sub_field('foto_mobile'); 
+			$fotoDesktop = get_sub_field('foto_desktop'); 
+			$texto = get_sub_field('texto'); 
+			$link = get_sub_field('link');
+			?>
+			<div class="carousel-item <?php echo ($i == 0) ? 'active' : ''; ?> ">
+				<img src="<?php echo $fotoDesktop['url']; ?>"  class="d-none d-md-block"  alt="<?php echo $fotoDesktop['alt'] ?>" />
+				<img src="<?php echo $fotoMobile['url']; ?>"  class="d-sm-block d-md-none"  alt="<?php echo $fotoMobile['alt'] ?>" />
+				<div class="carousel-caption text-left">
+					<h5><?php echo $texto; ?></h5>
+					<div class="clearfix"></div>
+					<a href="<?php echo $link; ?>" class="btn btn-info"><?php echo pll_e('Saiba Mais'); ?> [+]</a>
 				</div>
-			<?php $i++;  endwhile ;  
-		endwhile; ?>
+			</div>
+			<?php $i++; endwhile;
+		endwhile;
+		?>
 	</div>
 	<a class="carousel-control-prev" href="#banners" role="button" data-slide="prev">
 		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -37,10 +36,3 @@
 		<span class="sr-only">Next</span>
 	</a>
 </div>
-
-
-
-
-
-
-
