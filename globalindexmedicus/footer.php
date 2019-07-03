2a43e41f3023b83587c690acdcc6f6a459a94f9b
da Silva <dasilvmar@paho.org>
2019-07-03 18:28:14 -0300

    Alterações Rodapé
    Modicações solicitadas, rodapé e footer

--- globalindexmedicus/footer.php
+++ globalindexmedicus/footer.php
@@ -2,20 +2,7 @@
 <footer id="footer" class="padding1">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-6">
-				<h5>Menu</h5>
-				<?php
-				wp_nav_menu( array(
-					'theme_location'    => 'rodape',
-					'depth'             => 1,
-					'container'         => 'ol',
-					'container_class'   => '',
-					'container_id'      => 'bs-example-navbar-collapse-1',
-					'menu_class'        => '',
-				) );
-				?>
-			</div>
-			<address id="footerAddress" class="col-md-6">
+			<address id="" class="col-md-6">
 				<h5><?php pll_e('Outros Índices'); ?></h5>
 				Avenue Appia 20 <br>
 				CH-1211 Geneva 27 <br>
@@ -23,8 +10,18 @@
 				Tel: (41) 22 791 41 50 <br>
 				<a href="http://www.who.int/library/en" target="_blank">http://www.who.int/library/en</a>
 			</address>
+			<address id="footerAddress" class="col-md-6">
+				<h5>Bireme</h5>
+				Rua Vergueiro, 1759 - 12º -	Vila Mariana <br>
+				São Paulo/SP - CEP: 04101-100 <br>
+				+55 11 5576-9800 <br>
+				<!-- <a href="http://www.who.int/library/en" target="_blank"></a> -->
+			</address>
 		</div>
 	</div>
 </footer>
-<div id="assFooter" class="text-center">BIREME | OPS | OMS</div>
+<div id="assFooter" class="text-center">
+	<?php $idioma = pll_current_language(); ?>
+	<img src="<?php bloginfo('template_directory') ?>/img/<?php echo $idioma; ?>/logoBireme.svg" alt="Bireme" class="img-fluid">
+</div>
 <?php wp_footer(); ?>
--- globalindexmedicus/js/main.js
+++ globalindexmedicus/js/main.js
@@ -1,7 +1,8 @@
-// icon Accordion
-
-$('.collapse').on('shown.bs.collapse', function(){
-	$(this).parent().find(".fa-plus").removeClass("fa-plus").addClass("fa-minus");
-}).on('hidden.bs.collapse', function(){
-	$(this).parent().find(".fa-minus").removeClass("fa-minus").addClass("fa-plus");
+jQuery( document ).ready( function( $ ) {
+	// icon Accordion
+	$('.collapse').on('shown.bs.collapse', function(){
+		$(this).parent().find(".fa-plus").removeClass("fa-plus").addClass("fa-minus");
+	}).on('hidden.bs.collapse', function(){
+		$(this).parent().find(".fa-minus").removeClass("fa-minus").addClass("fa-plus");
+	});
 });
