7e328912a681f1545786f3b2c833877037bf2626
da Silva <dasilvmar@paho.org>
2019-07-04 13:47:06 -0300

    Widgets Ajutes
    Formatação de font e listas

--- globalindexmedicus/includes/widgets.php
+++ globalindexmedicus/includes/widgets.php
@@ -2,39 +2,37 @@
 	<div class="container">
 		<div class="row">
 			<!-- Últimos Artigos -->
-			<div class="col-md-4">
+			<div class="col-md-4 widgetColumn">
 				<ul class="list-unstyled">
 				<?php dynamic_sidebar('gim_widgets1'); ?>
 				</ul>
 			</div>
 			<!-- Últimos Artigos -->
-			<div class="col-md-4">
+			<div class="col-md-4 widgetColumn">
 				<ul class="list-unstyled">
 				<?php dynamic_sidebar('gim_widgets2'); ?>
 				</ul>
 			</div>
 			<!-- Mais Visualizados -->
-			<div class="col-md-4">
+			<div class="col-md-4 widgetColumn">
 				<ul class="list-unstyled">
 				<?php dynamic_sidebar('gim_widgets3'); ?>
 				</ul>
 			</div>
-			<br>
-			<div style="width: 100%; height: 2px; background: white;" ></div>
-			<br>
-			<div class="col-md-4">
+			<div class="widgetDivisao"></div>
+			<div class="col-md-4 widgetColumn">
 				<ul class="list-unstyled">
 				<?php dynamic_sidebar('gim_widgets4'); ?>
 				</ul>
 			</div>
 			<!-- Últimos Artigos -->
-			<div class="col-md-4">
+			<div class="col-md-4 widgetColumn">
 				<ul class="list-unstyled">
 				<?php dynamic_sidebar('gim_widgets5'); ?>
 				</ul>
 			</div>
 			<!-- Mais Visualizados -->
-			<div class="col-md-4">
+			<div class="col-md-4 widgetColumn">
 				<ul class="list-unstyled">
 				<?php dynamic_sidebar('gim_widgets6'); ?>
 				</ul>
