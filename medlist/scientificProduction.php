8f7dc7c995747cab42b5cb51d6159f8849912802
da Silva <dasilvmar@paho.org>
2019-08-16 15:49:35 -0300

    Título fora da coluna
    Conforme solicitado o título foi inserido fora da coluna.

--- medlist/scientificProduction.php
+++ medlist/scientificProduction.php
@@ -1,16 +1,20 @@
 <div class="tab-pane fade" id="servico2" role="tabpanel" aria-labelledby="profile-tab">
-	<div class="row">
-		<div class="col-md-6 boxOver">
-			<h5>Coluna 1</h5>
-			<a href=""><b>Cabazitaxel, abiraterona y enzalutamida en cáncer de próstata metastásico resistente a la castración con fracaso a docetaxel</b><br>
-			INTRODUCCIÓN: El cáncer de próstata es la neoplasia maligna no cutánea más frecuente en varones. En estadíos tempranos el tratamiento es quirúrgico y radioterapéutico mientras que si la enfermedad está localmente avanzada o hay metástasis los pacientes son tratados con terapia anti-androgénica...</a>
-			<hr>
-		</div>
-		<div class="col-md-6 boxOver">
-			<h5>Coluna 2</h5>
+<div class="row">
+	<div class="col-md-6">
+		<h5>Coluna 1</h5>
+		<div class="boxOver">
 			<a href=""><b>Cabazitaxel, abiraterona y enzalutamida en cáncer de próstata metastásico resistente a la castración con fracaso a docetaxel</b><br>
 			INTRODUCCIÓN: El cáncer de próstata es la neoplasia maligna no cutánea más frecuente en varones. En estadíos tempranos el tratamiento es quirúrgico y radioterapéutico mientras que si la enfermedad está localmente avanzada o hay metástasis los pacientes son tratados con terapia anti-androgénica...</a>
 			<hr>
 		</div>
 	</div>
+	<div class="col-md-6">
+		<h5>Coluna 2</h5>
+		<div class="boxOver">
+			<a href=""><b>Cabazitaxel, abiraterona y enzalutamida en cáncer de próstata metastásico resistente a la castración con fracaso a docetaxel</b><br>
+			INTRODUCCIÓN: El cáncer de próstata es la neoplasia maligna no cutánea más frecuente en varones. En estadíos tempranos el tratamiento es quirúrgico y radioterapéutico mientras que si la enfermedad está localmente avanzada o hay metástasis los pacientes son tratados con terapia anti-androgénica...</a>
+			<hr>
+		</div>
+	</div>
+</div>
 </div>
