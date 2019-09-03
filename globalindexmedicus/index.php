84e759b1b942eacc29bd0165986315d55ff8aea6
da Silva <dasilvmar@paho.org>
2019-09-03 18:13:26 -0300

    How To
    How to como arquivo index.php

--- globalindexmedicus/index.php
+++ globalindexmedicus/index.php
@@ -12,20 +12,41 @@
 				// 'posts_per_page' => 6,
 				'post_type' => 'biblioteca',
 				'orderby' => 'title',
-    			'order'   => 'ASC'
+				'order'   => 'ASC'
 			));
 			while($biblioteca->have_posts()) : $biblioteca->the_post();
 				?>
 				<artigle class="col-12 col-sm-6 col-md-4 bibliotecaHome">
 					<a href="<?php the_permalink(); ?>">
-						 <?php the_post_thumbnail('Bibliotecas', array('class'=>'img-fluid')); ?>
+						<?php the_post_thumbnail('Bibliotecas', array('class'=>'img-fluid')); ?>
 						<h4><span><?php the_title(); ?></span></h4>
-						<?php echo substr(get_the_excerpt(), 0, 150).'...'; ?>
+						<?php the_excerpt(); ?>
 					</a> <br><br>
 				</artigle>
 			<?php endwhile; ?>
+
+			<!-- Fixo -->
+			<?php
+				$args = array(
+					'post_type' => 'page',
+					'name' => 'home-en', 'home-pt','home-es','home-ru','home-zh','home-fr','home-ar'
+				); 
+				$query = new WP_Query( $args ); 
+				if ( $query->have_posts() ) { 
+					while ( $query->have_posts() ) {
+						$query->the_post(); 
+					}
+				} 
+			?>
+			<artigle class="col-12 col-sm-6 col-md-4 bibliotecaHome">
+				<a href="<?php the_permalink(); ?>">
+					<?php the_post_thumbnail('Bibliotecas', array('class'=>'img-fluid')); ?>
+					<h4><span><?php the_title(); ?></span></h4>
+					<?php the_excerpt(); ?>
+				</a> <br><br>
+			</artigle>
 		</div>
 	</div>
 </section>
-<?php get_template_part( 'includes/widgets') ?>
+<?php get_template_part('includes/widgets') ?>
 <?php get_footer() ?>
