2a0d7ffedebf5461a131c47e573b58ab3fd06e74
da Silva <dasilvmar@paho.org>
2019-09-20 09:02:17 -0300

    Termo de Uso
    Inserir Termo de uso

--- globalindexmedicus/footer.php
+++ globalindexmedicus/footer.php
@@ -18,7 +18,22 @@
 	</div>
 </footer>
 <div id="assFooter" class="text-center">
-	<?php $idioma = pll_current_language(); ?>
-	<img src="<?php bloginfo('template_directory') ?>/img/<?php echo $idioma; ?>/logoBireme.svg" alt="Bireme" class="img-fluid">
+	<div class="container">
+		<?php $idioma = pll_current_language(); ?>
+		<img src="<?php bloginfo('template_directory') ?>/img/<?php echo $idioma; ?>/logoBireme.svg" alt="Bireme" class="img-fluid">
+		<hr>
+		<div class="row" id="footerTermos">
+			<div class="col-md-4 text-left">
+				Powered by iAHx - <?php bloginfo('name');?>
+			</div>
+			<div class="col-md-4 text-center">
+				<a href="http://feedback.bireme.org/feedback/?application=iahx&version=2.0&lang=<?php echo $idioma; ?>&site=portal" target="_blank"><?php pll_e('Enviar um comentário'); ?></a>
+			</div>
+			<div class="col-md-4 text-right">
+				<a href="http://politicas.bireme.org/terminos/<?php echo $idioma; ?>/" target="_blank"><?php pll_e('Termos e condições de uso'); ?></a> |
+				<a href="http://politicas.bireme.org/privacidad/<?php echo $idioma; ?>/" target="_blank"><?php pll_e('Políticas de privacidade'); ?></a>
+			</div>
+		</div>
+	</div>
 </div>
 <?php wp_footer(); ?>
