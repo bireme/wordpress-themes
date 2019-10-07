f6bdc8ebef2901d20d5d2ddd6fa1e3a3b6092952
da Silva <dasilvmar@paho.org>
2019-10-07 16:08:06 -0300

    Página 404
    Mudanças nas página 404

--- decs/404.php
+++ decs/404.php
@@ -8,8 +8,16 @@
 <main id="main_container" class="padding1">
 	<div class="container">
 		<p class="text-justify">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus provident impedit iure officia inventore quo, rerum, doloribus et perferendis vero excepturi consequuntur asperiores recusandae minima iste ab magni porro eligendi. Accusamus at veniam pariatur illo animi atque doloribus facilis eius excepturi id, quas repudiandae perferendis velit totam dolor officia repellat, nulla minus error sequi, exercitationem rerum provident ratione praesentium eaque! Rerum praesentium voluptas totam nihil blanditiis! A non cumque laudantium reprehenderit. Quisquam esse, saepe soluta quos, architecto eaque earum sunt ut a fugiat necessitatibus repellat, asperiores vel inventore? Reiciendis, eius nesciunt, eveniet ullam repudiandae iusto, quaerat totam officia maiores animi natus corporis consequuntur dolorum asperiores sint. Vero amet ducimus officia labore illo vitae delectus nesciunt quasi. Cumque non quasi consectetur, impedit labore inventore fugit tempore eum incidunt nemo harum nam sequi quibusdam id ab magni et nihil dolorum qui totam? Quia, eos ab voluptate, vel explicabo, natus pariatur quaerat culpa animi esse quas nulla. Tempore earum architecto perspiciatis necessitatibus vero eius inventore natus illum accusantium quasi cum corrupti nisi maiores praesentium, molestiae cupiditate voluptatum facilis. Totam, quasi. Iusto dignissimos pariatur perspiciatis nulla nostrum nesciunt enim. Dolorum maiores illum aliquam praesentium architecto porro, facere quaerat sequi excepturi voluptatem, nulla expedita quisquam consectetur ab temporibus doloremque saepe sunt numquam vero, ipsam repellat eos reprehenderit eius, id eaque. Odio doloribus possimus error illum ut est similique quis labore ducimus nisi dolor sequi saepe obcaecati vitae sapiente harum, a molestiae beatae quidem soluta quaerat. Culpa quae eaque, reiciendis provident sapiente illo? Quaerat eius dolor debitis deserunt fugit reprehenderit non, dolore in, ut. Nobis placeat ratione atque ex vero iure, dolorum beatae consequuntur doloremque praesentium corrupti numquam doloribus quae odio voluptatibus, labore necessitatibus. Dolorem quo, ducimus ipsam similique, consequuntur quod vero aliquam facere perspiciatis reiciendis sit doloribus magni, velit fugit itaque laboriosam beatae alias, doloremque.
+			<br>
+			Página não encontrada!
 		</p>
 	</div>
 </main>
+<?php if( is_active_sidebar('home_widget') ) { ?>
+     <section class="container" id="widgetHome">
+         <?php dynamic_sidebar('home_widget'); ?>
+     </section>
+<?php } ?>
+<?php get_template_part('includes/partners') ?>
+</section>
 <?php get_footer(); ?>
