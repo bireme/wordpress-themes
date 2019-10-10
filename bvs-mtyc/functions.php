94a7ce25ec0b2dda6daf96590887a142295afa66
Wilson Moura <wilsonsmoura@gmail.com>
2019-10-10 16:53:42 -0300

    Theme Eventchamp: Update functions.php

--- bvs-mtyc/functions.php
+++ bvs-mtyc/functions.php
@@ -1,14 +1,25 @@
 <?php
-    function theme_enqueue_styles() {
+    if ( ! function_exists('theme_enqueue_styles') ) {
+        function theme_enqueue_styles() {
+            $parent_style = 'parent-style';
 
-        $parent_style = 'parent-style';
-
-        wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-        wp_enqueue_style( 'child-style',
-            get_stylesheet_directory_uri() . '/style.css',
-            array( $parent_style ),
-            wp_get_theme()->get('Version')
-        );
+            wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+            wp_enqueue_style( 'child-style',
+                get_stylesheet_directory_uri() . '/style.css',
+                array( $parent_style ),
+                wp_get_theme()->get('Version')
+            );
+        }
+        add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
     }
-    add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+    if ( ! function_exists('https_request_local') ) {
+        function https_request_local( $args, $url ) {
+            if ( preg_match('/xml|rss|feed/', $url) ) {
+                $args['reject_unsafe_urls'] = false;
+            }
+            return $args;
+        }
+        add_filter( 'http_request_args', 'https_request_local', 5, 2 );
+    }
 ?>
