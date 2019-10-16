8f30be8845a9329a3bed68342dd70a7ef9085210
da Silva <dasilvmar@paho.org>
2019-10-16 14:40:45 -0300

    Título Modal Lista de Paises
    Foi inserido códgo para tradução do titulo do modal.

--- e-BlueInfo/includes/modais.php
+++ e-BlueInfo/includes/modais.php
@@ -42,7 +42,7 @@
 	<div class="modal-dialog modal-dialog-centered" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title" id="googlePlayTitle">Paises Interessados</h5>
+				<h5 class="modal-title" id="googlePlayTitle"><?php pll_e('Interested Countries'); ?></h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
