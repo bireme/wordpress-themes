3590653316734e860b556bd2e4b3ad5ce81de3b7
wilsonmoura <wilsonsmoura@gmail.com>
2019-10-22 11:49:21 -0300

    Minor fixes

--- e-BlueInfo/footer.php
+++ e-BlueInfo/footer.php
@@ -5,11 +5,11 @@
 		<div class="row">
 			<div class="col-md-5">
 				<b>e-BlueInfo</b> <br>
-				<a href="http://politicas.bireme.org/terminos/" target="_blank"><?php pll_e('Terms and conditions of use'); ?></a> | 
-				<a href="http://politicas.bireme.org/privacidad/" target="_blank"><?php pll_e('Privacy policy'); ?></a>
+				<a href="https://politicas.bireme.org/terminos/" target="_blank"><?php pll_e('Terms and conditions of use'); ?></a> | 
+				<a href="https://politicas.bireme.org/privacidad/" target="_blank"><?php pll_e('Privacy policy'); ?></a>
 			</div>
 			<div class="col-md-7 text-right" id="logoOPAS">
-				<img src="http://logos.bireme.org/img/<?php echo $idioma; ?>/h_bir_color.svg" alt="">
+				<img src="https://logos.bireme.org/img/<?php echo $idioma; ?>/h_bir_color.svg" alt="">
 			</div>
 		</div>
 	</div>
