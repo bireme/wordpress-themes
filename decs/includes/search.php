4e5320074092f2f9f5145035ab5b18bbac2bc0c3
da Silva <dasilvmar@paho.org>
2019-10-23 17:33:35 -0300

    Ajustes DeCS Link Home + Campo de Busca
    Foi corrigido o tamanho do select no campo de busca (grid)
    Foi ajustado link para hoje em todos os idiomas

--- decs/includes/search.php
+++ decs/includes/search.php
@@ -4,7 +4,7 @@
 		<div class="headerSearch" >
 			<form action="<?php echo real_site_url($ths_plugin_slug) . 'ths'; ?>">
 				<div class="row">
-					<div class="col-md-4 col-lg-3 selectBoxSearch">
+					<div class="col-md-5 col-lg-4 selectBoxSearch">
 						<select name="filter" id="filter" class="formSelect">
 							<option value="ths_termall"><?php pll_e('All Descriptor Terms'); ?></option>
 							<option value="ths_preferredterm"><?php pll_e('Main Heading (Descriptor) Terms'); ?></option>
@@ -15,7 +15,7 @@
 							<option value="ths_qualifall"><?php pll_e('All Qualifier Terms'); ?></option>
 						</select>
 					</div>
-					<div class="col-md-6 col-lg-8 inputBoxSearch">
+					<div class="col-md-5 col-lg-7 inputBoxSearch">
 						<input type="text" name="q" placeholder="<?php pll_e('Search'); ?>" id="fieldSearch" autocomplete="off" >
 						<a id="speakBtn" href="#"><i class="fas fa-microphone-alt"></i></a>
 					</div>
