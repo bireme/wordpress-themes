cd6945abaaf4f76e595982f0f15d0592a5ac6e05
da Silva <dasilvmar@paho.org>
2019-10-24 12:07:22 -0300

    Correções
    Barra de Acessibilidade
    - functions.php / topAccessibility.php
    Termos e Uso
    - Inserir https e idiomas - footer.php
    Link Home pelo logo
    - header-in.php

--- e-BlueInfo/footer.php
+++ e-BlueInfo/footer.php
@@ -5,8 +5,8 @@
 		<div class="row">
 			<div class="col-md-5">
 				<b>e-BlueInfo</b> <br>
-				<a href="https://politicas.bireme.org/terminos/" target="_blank"><?php pll_e('Terms and conditions of use'); ?></a> | 
-				<a href="https://politicas.bireme.org/privacidad/" target="_blank"><?php pll_e('Privacy policy'); ?></a>
+				<a href="https://politicas.bireme.org/terminos/<?php echo $idioma; ?>" target="_blank"><?php pll_e('Terms and conditions of use'); ?></a> | 
+				<a href="https://politicas.bireme.org/privacidad/<?php echo $idioma; ?>" target="_blank"><?php pll_e('Privacy policy'); ?></a>
 			</div>
 			<div class="col-md-7 text-right" id="logoOPAS">
 				<img src="https://logos.bireme.org/img/<?php echo $idioma; ?>/h_bir_color.svg" alt="">
--- e-BlueInfo/header-in.php
+++ e-BlueInfo/header-in.php
@@ -6,11 +6,12 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,user-scalable=1" /> 
 	<?php wp_head(); ?>
 </head>
+<?php $idioma = pll_current_language(); ?>
 <header class="" id="headerIn">
 	<div class="container">
 		<div class="row">
 			<div class="col-12 col-md-12 col-lg-4" id="logoIn">
-				<a href="<?php bloginfo('url') ?>"><img src="<?php bloginfo('template_directory') ?>/img/logo.png" alt="Brand e-BlueInfo"></a>
+				<a href="<?php echo get_option('siteurl'); ?>/<?php echo $idioma=='pt'?'':$idioma; ?>"><img src="<?php bloginfo('template_directory') ?>/img/logo.png" alt="Brand e-BlueInfo"></a>
 			</div>
 			<div class="col-12 col-md-12 col-lg-8">
 				<div id="lang">
