55800fdd240e67ac8f1b37ba879dd1892eb32b11
da Silva <dasilvmar@paho.org>
2020-01-13 11:03:27 -0300

    Ajustes  CSS Acessibilidade
    Ajustes no css

--- SAA2019/includes/topAcessibility.php
+++ SAA2019/includes/topAcessibility.php
@@ -4,15 +4,15 @@
 			<div class="col-md-6" id="acessibilidadeTutorial">
 				<a href="#main_container" tabindex="1">Conteúdo Principal <span class="hiddenMobile">1</span></a>
 				<a href="#nav" tabindex="2">Menu <span class="hiddenMobile">2</span></a>
-				<a href="#fieldSearch" tabindex="3">Busca <span class="hiddenMobile">3</span></a>
+				<a href="#fieldSearch" tabindex="3" id="accessibilitySearch">Busca <span class="hiddenMobile">3</span></a>
 				<a href="#footer" tabindex="4">Rodapé <span class="hiddenMobile">4</span></a>
 			</div>
 			<div class="col-md-6" id="acessibilidadeFontes">
 				<a id="fontPlus"  tabindex="5">+A</a> | 
 				<a id="fontNormal"  tabindex="6">A</a> | 
 				<a id="fontLess"  tabindex="7">-A</a> | 
-				<a id="contraste"  tabindex="8"><i class="fas fa-adjust"></i> Alto Contraste</a>
-				<!-- | <a id="acessibilidade" class="" tabindex="9" href="docAcessibilidade.php"><i class="fas fa-wheelchair"></i> Mais Acessibilidades</a> -->
+				<a id="contraste"  tabindex="8"><i class="fas fa-adjust"></i> Alto Contraste</a> <!--| 
+				<a href="#" id="acessibilidade" class="" tabindex="9" href="docAcessibilidade.php"><i class="fas fa-wheelchair"></i> Accessiblity</a-->
 			</div>
 		</div>
 	</div>
