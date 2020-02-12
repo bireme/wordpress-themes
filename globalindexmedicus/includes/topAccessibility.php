a3db917e1137ec8709c1748205d80f5dd0dccee8
da Silva <dasilvmar@paho.org>
2020-02-12 14:51:47 -0300

    Ajuste para leitores de tela

--- globalindexmedicus/includes/topAccessibility.php
+++ globalindexmedicus/includes/topAccessibility.php
@@ -9,10 +9,10 @@
 				<a href="#footer" tabindex="4" role="button"><?php pll_e('Footer'); ?> <span class="hiddenMobile">4</span></a>
 			</div>
 			<div class="col-md-6" id="acessibilidadeFontes">
-				<a href="#!" role="button" id="fontPlus" tabindex="5">+A</a>
-				<a href="#!" role="button" id="fontNormal" tabindex="6">A</a>
-				<a href="#!" role="button" id="fontLess" tabindex="7">-A</a>
-				<a href="#!" role="button" id="contraste" tabindex="8"><i class="fas fa-adjust"></i> <?php pll_e('High contrast'); ?></a>
+				<a href="#!" role="button" id="fontPlus" tabindex="5" aria-hidden="true">+A</a>
+				<a href="#!" role="button" id="fontNormal" tabindex="6" aria-hidden="true">A</a>
+				<a href="#!" role="button" id="fontLess" tabindex="7" aria-hidden="true">-A</a>
+				<a href="#!" role="button" id="contraste" tabindex="8" aria-hidden="true"><i class="fas fa-adjust"></i> <?php pll_e('High contrast'); ?></a>
 				<a href="https://politicas.bireme.org/accesibilidad/<?php echo ( in_array($idioma, array('ar', 'fr', 'ru', 'zh')) ? "en" : $idioma ); ?>" role="button" id="accebilidade" tabindex="9" target="_blank"><i class="fas fa-wheelchair"></i></a>
 			</div>
 		</div>
