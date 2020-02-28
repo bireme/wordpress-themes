759b549240ed9b1426b12d7e3ce8f7977113290a
wilsonmoura <wilsonsmoura@gmail.com>
2020-02-28 17:07:44 -0300

    Relator FUNASA theme: minor fixes

--- video-relatos-funasa/single-relatos_funasa.php
+++ video-relatos-funasa/single-relatos_funasa.php
@@ -28,68 +28,80 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div id="content" class="single1column" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
-
-                                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                                        <header class="entry-header">
-                                                <?php if ( is_single() ) : ?>
-                                                        <h1 class="entry-title"><?php the_title(); ?></h1>
-                                                <?php endif; // is_single() ?>
-                                                <?php if ( comments_open() ) : ?>
-                                                        <div class="comments-link">
-                                                                <?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
-                                                        </div><!-- .comments-link -->
-                                                <?php endif; // comments_open() ?>
-                                        </header><!-- .entry-header -->
-                                        <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-                                                <?php if ( is_single() ) : ?>
-                                                <div class="entry-content"><div class="r-block-title">Objetivo Geral</div>
-                                                        <?php the_content(); ?>
-                                                </div>
-                                                <?php endif; ?>
-                                        </div>
-                                          <div class="related_links r-block">
-                                                <div class="r-block-title">Instituição executora</div>
-                                                <?php //$key="links_relacionados"; echo get_post_meta($post->ID, $key, true); ?>
-                                                <?php echo pods_field( "relatos_funasa", $post->ID, 'instituio_executora', true ); ?>
-                                            </div>
-                                            <div class="related_links r-block">
-                                                <div class="r-block-title">Coordenador(a) do projeto de pesquisa</div>
-                                                <?php //$key="links_relacionados"; echo get_post_meta($post->ID, $key, true); ?>
-                                                <?php echo pods_field( "relatos_funasa", $post->ID, 'coordenadora_do_projeto_de_pesquisa', true ); ?>
-                                            </div>
-             
-                                            <div class="related_links r-block"><div class="r-block-title">Imagem</div>
-                                                <?php //$key="links_relacionados"; echo get_post_meta($post->ID, $key, true); ?>
-                                                <?php echo pods_field( "relatos_funasa", $post->ID, 'imagem', true ); ?>
-                                            </div>
-                                            <div class="related_links r-block">
-                                                <div class="r-block-title">Produto</div>
-                                                <?php //$key="links_relacionados"; echo get_post_meta($post->ID, $key, true); ?>
-                                                <?php echo pods_field( "relatos_funasa", $post->ID, 'produto', true ); ?>
-                                            </div>
-                                             <div class="related_links r-block">
-                                                <div class="r-block-title">Publicação</div>
-                                                <?php //$key="links_relacionados"; echo get_post_meta($post->ID, $key, true); ?>
-                                                <?php echo pods_field( "relatos_funasa", $post->ID, 'publicao', true ); ?>
-                                            </div>
-                                            <div class="related_links r-block">
-                                                <div class="r-block-title">Links relacionados</div>
-                                                <?php //$key="links_relacionados"; echo get_post_meta($post->ID, $key, true); ?>
-                                                <?php echo pods_field( "relatos_funasa", $post->ID, 'links_relacionados', true ); ?>
-                                            </div>
-					
-                                </article>
-                                <?php comments_template( '', true ); ?>
-                        <?php endwhile; // end of the loop. ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
+                        <?php if ( is_single() ) : ?>
+                            <h1 class="entry-title"><?php the_title(); ?></h1>
+                        <?php endif; // is_single() ?>
+                        <?php if ( comments_open() ) : ?>
+                            <div class="comments-link">
+                                <?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
+                            </div><!-- .comments-link -->
+                        <?php endif; // comments_open() ?>
+                    </header><!-- .entry-header -->
+                    <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+                        <?php if ( is_single() ) : ?>
+                        <div class="entry-content">
+                            <div class="r-block-title">Objetivo Geral</div>
+                            <?php the_content(); ?>
+                        </div>
+                        <?php endif; ?>
+                    </div>
+                    <?php if ( function_exists('pods_field') ) : ?>
+                        <?php $pod = pods_field( "relatos_funasa", $post->ID, 'instituicao_executora', true ); ?>
+                        <?php if ( $pod ) : ?>
+                            <div class="related_links r-block">
+                                <div class="r-block-title">Instituição executora</div>
+                                <?php echo $pod; ?>
+                            </div>
+                        <?php endif; ?>
+                        <?php $pod = pods_field( "relatos_funasa", $post->ID, 'coordenadora', true ); ?>
+                        <?php if ( $pod ) : ?>
+                            <div class="related_links r-block">
+                                <div class="r-block-title">Coordenador(a) do projeto de pesquisa</div>
+                                <?php echo $pod; ?>
+                            </div>
+                        <?php endif; ?>
+                        <?php $pod = pods_field( "relatos_funasa", $post->ID, 'imagem', true ); ?>
+                        <?php if ( $pod ) : ?>
+                            <div class="related_links r-block"><div class="r-block-title">Imagem</div>
+                                <?php echo $pod; ?>
+                            </div>
+                        <?php endif; ?>
+                        <?php $pod = pods_field( "relatos_funasa", $post->ID, 'produto', true ); ?>
+                        <?php if ( $pod ) : ?>
+                            <div class="related_links r-block">
+                                <div class="r-block-title">Produto</div>
+                                <?php echo $pod; ?>
+                            </div>
+                        <?php endif; ?>
+                        <?php $pod = pods_field( "relatos_funasa", $post->ID, 'publicacao', true ); ?>
+                        <?php if ( $pod ) : ?>
+                            <div class="related_links r-block">
+                                <div class="r-block-title">Publicação</div>
+                                <?php echo $pod; ?>
+                            </div>
+                        <?php endif; ?>
+                        <?php $pod = pods_field( "relatos_funasa", $post->ID, 'links_relacionados', true ); ?>
+                        <?php if ( $pod ) : ?>
+                            <div class="related_links r-block">
+                                <div class="r-block-title">Links relacionados</div>
+                                <?php echo $pod; ?>
+                            </div>
+                        <?php endif; ?>
+                    <?php endif; ?>
+                </article>
+                <?php comments_template( '', true ); ?>
+            <?php endwhile; // end of the loop. ?>
 		</div><!-- #content --> 
 		<div class="single2column">
 			 <div class="related_links r-block">
-                                        <div class="r-block-title">Projeto de pesquisa</div>
-                                            <?php //$key="links_relacionados"; echo get_post_meta($post->ID, $key, true); ?>
-                                            <?php echo get_the_term_list( $post->ID, 'projeto_de_pesquisa', ' ', ', ' ); ?>
-                                        <div class="r-block-title">Edital</div>
-                                            <?php //$key="links_relacionados"; echo get_post_meta($post->ID, $key, true); ?>
-                                            <?php echo get_the_term_list( $post->ID, 'edital', ' ', ', ' ); ?>
+                <div class="r-block-title">Projeto de pesquisa</div>
+                    <?php //$key="links_relacionados"; echo get_post_meta($post->ID, $key, true); ?>
+                    <?php echo get_the_term_list( $post->ID, 'projeto_de_pesquisa', ' ', ', ' ); ?>
+                <div class="r-block-title">Edital</div>
+                    <?php //$key="links_relacionados"; echo get_post_meta($post->ID, $key, true); ?>
+                    <?php echo get_the_term_list( $post->ID, 'edital', ' ', ', ' ); ?>
              </div>                           
 		</div>
 	</div><!-- #primary -->
