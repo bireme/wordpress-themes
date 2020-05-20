7da56d44f7f9232fb4bfe73ff6b08e11ed6fe35f
wilsonmoura <wilsonsmoura@gmail.com>
2020-05-20 15:35:33 -0300

    Relatos FUNASA theme: Acessibility bar

---
+++ video-relatos-funasa/barra-acessibilidade.php
@@ -0,0 +1,18 @@
+<div class="containerCabecalho">
+    <header class="containerTopo">
+        <div class="topo">
+            <div class="marca">
+                <a target="_blank" href="http://www.funasa.gov.br"><img src="http://rcfunasa.bvsalud.org/themes/Mirage2FUNASA/images/marcaFunasa.png" class="logoFunasa"></a>
+            </div>
+            <nav class="menuAcessibilidade">
+                <ul class="nav nav-pills pull-right">
+                    <div class="pull-left idoptions">
+                        <li>
+                            <a class="hidden-xs" id="btnContrast" href="#">Contraste</a>
+                        </li>
+                    </div>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</div>
