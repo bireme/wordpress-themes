7ab67b727c90e89fc920a3dcbbfc96e561feb121
da Silva <dasilvmar@paho.org>
2020-09-10 09:39:13 -0300

    Ajuda para imagens indisponíveis de categorias

--- SAA2019/category.php
+++ SAA2019/category.php
@@ -10,7 +10,12 @@
 				<?php while(have_posts()) : the_post(); ?>
 					<article class="col-md-4 categoryHome imEffect">
 						<a href="<?php permalink_link(); ?>">
-							<?php the_post_thumbnail('banners',['class' => 'img-fluid  imgPost']); ?>
+							<?php 
+							if ( has_post_thumbnail()) {
+								the_post_thumbnail('banners',['class' => 'img-fluid']);
+							}else{ ?>
+								<img src="<?php bloginfo( 'template_directory')?>/img/indisponivel.jpg" class="img-fluid" alt="">
+							<?php }	 ?>
 							<b><?php the_title(); ?></b> <br>
 							<small><?php echo human_time_diff(get_the_time('U'), current_time('timestamp')) . ' atrás'; ?></small>
 						</a><br><br>
