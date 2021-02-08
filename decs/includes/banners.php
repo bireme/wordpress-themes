8e1326308b0fcabc9322c60b55825f4880d9180b
falbrito <britofa@paho.org>
2021-02-08 13:53:04 -0300

    desbloqueio de banner

--- decs/includes/banners.php
+++ decs/includes/banners.php
@@ -29,13 +29,13 @@
 				endwhile;?>
 			</div>
 		</div>
-		<!-- <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+		<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
 			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 			<span class="sr-only">Previous</span>
 		</a>
 		<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
 			<span class="carousel-control-next-icon" aria-hidden="true"></span>
 			<span class="sr-only">Next</span>
-		</a> -->
+		</a>
 	</div>
 </section>
