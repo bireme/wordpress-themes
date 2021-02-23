5413ba0ee198936d3f795f9470bb6700655819ce
da Silva <dasilvmar@paho.org>
2021-02-23 17:42:11 -0300

    Ajuste acessibilidades

--- evideasy/includes/topAcessibility.php
+++ evideasy/includes/topAcessibility.php
@@ -4,8 +4,8 @@
 		<div class="row">
 			<div class="col-md-6" id="accessibilityTutorial">
 				<a href="#main_container" tabindex="1" role="button"><?php pll_e('Main content'); ?> <span class="hiddenMobile">1</span></a>
-				<a href="#nav" tabindex="2" role="button"><?php pll_e('Menu'); ?> <span class="hiddenMobile">2</span></a>
-				<a href="#fieldSearch" tabindex="3" id="accessibilitySearch" role="button"><?php pll_e('Search'); ?> <span class="hiddenMobile">3</span></a>
+				<!-- <a href="#nav" tabindex="2" role="button"><?php pll_e('Menu'); ?> <span class="hiddenMobile">2</span></a> -->
+				<!-- <a href="#fieldSearch" tabindex="3" id="accessibilitySearch" role="button"><?php pll_e('Search'); ?> <span class="hiddenMobile">3</span></a> -->
 				<a href="#footer" tabindex="4" role="button"><?php pll_e('Footer'); ?> <span class="hiddenMobile">4</span></a>
 			</div>
 			<div class="col-md-6" id="accessibilityFontes">
