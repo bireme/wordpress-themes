6883724f020ab86443d4e660c5e774fe3fdd7678
wilsonmoura <wilsonsmoura@gmail.com>
2021-03-22 19:35:03 -0300

    Fixed #45 -- e-BlueInfo theme: Deactivate template parts in e-BlueInfo app

--- e-BlueInfo/header-in.php
+++ e-BlueInfo/header-in.php
@@ -6,42 +6,46 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,user-scalable=1" />
 	<?php wp_head(); ?>
 </head>
+
 <?php $idioma = pll_current_language(); ?>
-<?php get_template_part('includes/topAccessibility') ?>
-<header class="" id="headerIn" role="banner">
-	<div class="container">
-		<div class="row">
-			<div class="col-12 col-md-12 col-lg-4" id="logoIn" role="logo">
-				<a href="<?php echo get_option('siteurl'); ?>/<?php echo $idioma=='pt'?'':$idioma; ?>"><img src="<?php bloginfo('template_directory') ?>/img/logo.png" alt="Brand e-BlueInfo"></a>
-			</div>	
-			<div class="col-12 col-md-12 col-lg-8" role="navigation">
-				<div id="lang">
-					<?php 
-					wp_nav_menu( array(
-						'theme_location'    => 'Language',	
-						'depth'             => 1,
-						'container'         => 'ul',
-						'container_class'   => 'list-unstyled',
-						'container_id'      => '',
-						'menu_class'        => '',
-					) );
-					?>
+
+<?php if ( !is_webview() ) : ?>
+	<?php get_template_part('includes/topAccessibility') ?>
+	<header class="" id="headerIn" role="banner">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-md-12 col-lg-4" id="logoIn" role="logo">
+					<a href="<?php echo get_option('siteurl'); ?>/<?php echo $idioma=='pt'?'':$idioma; ?>"><img src="<?php bloginfo('template_directory') ?>/img/logo.png" alt="Brand e-BlueInfo"></a>
+				</div>	
+				<div class="col-12 col-md-12 col-lg-8" role="navigation">
+					<div id="lang">
+						<?php 
+						wp_nav_menu( array(
+							'theme_location'    => 'Language',	
+							'depth'             => 1,
+							'container'         => 'ul',
+							'container_class'   => 'list-unstyled',
+							'container_id'      => '',
+							'menu_class'        => '',
+						) );
+						?>
+					</div>
+				</div>
+				<div class="col-12">
+					<nav id="nav" role="navigation">
+						<?php
+						wp_nav_menu( array(
+							'theme_location'    => 'main-nav',
+							'depth'             => 1,
+							'container'         => 'ul',
+							'container_class'   => 'list-unstyle',
+							'container_id'      => '',
+							'menu_class'        => '',
+						) );
+						?>
+					</nav>
 				</div>
 			</div>
-			<div class="col-12">
-				<nav id="nav" role="navigation">
-					<?php
-					wp_nav_menu( array(
-						'theme_location'    => 'main-nav',
-						'depth'             => 1,
-						'container'         => 'ul',
-						'container_class'   => 'list-unstyle',
-						'container_id'      => '',
-						'menu_class'        => '',
-					) );
-					?>
-				</nav>
-			</div>
 		</div>
-	</div>
-</header>
+	</header>
+<?php endif; ?>
