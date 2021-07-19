57893b9c5ff5bbca0e6f31a65a5916a50bf38244
Márcio Alves <marcioalvessilvajc@gmail.com>
2021-07-19 15:51:44 -0300

    Ajustes na página de diretórios de instituições

--- bibliosus/instituicoes.php
+++ bibliosus/instituicoes.php
@@ -7,41 +7,60 @@
 		<p><?php the_content(); ?></p>
 		
 		<br><hr><br>
-
-		<?php 
-		$posts = new WP_Query([
-			'post_type' => 'instituicoes',
-			'orderby' => 'title',
-			'order' => 'ASC',
-			'posts_per_page' => '-1'
-		]);
-		while($posts->have_posts()):$posts->the_post();
-			$nome_da_unidade 		= get_field('$nome_da_unidade'); 
-			$cnpj 					= get_field('cnpj'); 
-			$endereco_completo 		= get_field('endereco_completo'); 
-			$cidade 				= get_field('cidade'); 
-			$uf 					= get_field('uf'); 
-			$cep 					= get_field('cep'); 
-			$telefones 				= get_field('telefones_-_ramal'); 
-			$email 					= get_field('e-mail'); 
-			$home_page 				= get_field('home_page'); 
-			$tipo_de_instituicao 	= get_field('tipo_de_instituicao'); 
-
-			nome_do_responsavel_por_este_cadastramento
-			?>
-			<article>
-				<b>Nome da Instituição: </b> <?php echo $nome_da_unidade; ?><br>
-				<b>CNPJ: </b><?php echo $cnpj; ?><br>
-				<b>Telefone: </b><?php echo $telefones; ?> - <b>E-mail: </b><?php echo $email; ?> <br>
-				<b>Link: </b><?php echo $home_page; ?><br>
-				<b>Endereço: </b><?php echo $endereco_completo; ?><br>
-				<b>Cidade: </b> <?php echo $cidade; ?> - <b>UF: </b><?php echo $uf; ?> - <b>CEP: <?php echo $cep; ?></b> <br>
-				<b>Tipo: </b><?php echo $tipo_de_instituicao; ?><br>
-				<hr>
-			</article>
-			<?php
-		endwhile;
-		?>
+		<div class="table-responsive">
+			<table class="table table-bordered table-striped">
+				<thead>
+					<tr>
+						<th>Nome da Instituição</th>
+						<th>CNPJ</th>
+						<th>Telefone</th>
+						<th>E-mail</th>
+						<th>Endereço</th>
+						<th>Cidade</th>
+						<th>UF</th>
+						<th>CEP</th>
+						<th>Página Web</th>
+						<th>Tipo</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php 
+					$posts = new WP_Query([
+						'post_type' => 'instituicoes',
+						'orderby' => 'title',
+						'order' => 'ASC',
+						'posts_per_page' => '-1'
+					]);
+					while($posts->have_posts()):$posts->the_post();
+						$nome_da_unidade 		= get_field('$nome_da_unidade'); 
+						$cnpj 					= get_field('cnpj'); 
+						$endereco_completo 		= get_field('endereco_completo'); 
+						$cidade 				= get_field('cidade'); 
+						$uf 					= get_field('uf'); 
+						$cep 					= get_field('cep'); 
+						$telefones 				= get_field('telefones_-_ramal'); 
+						$email 					= get_field('e-mail'); 
+						$home_page 				= get_field('home_page'); 
+						$tipo_de_instituicao 	= get_field('tipo_de_instituicao');
+						?>
+						<tr>
+							<td><?php echo $nome_da_unidade; ?>></td>
+							<td><?php echo $cnpj; ?></td>
+							<td><?php echo $telefones; ?></td>
+							<td><?php echo $email; ?></td>
+							<td><?php echo $endereco_completo; ?></td>
+							<td><?php echo $cidade; ?></td>
+							<td><?php echo $uf; ?></td>
+							<td><?php echo $cep; ?></td>
+							<td><a href="<?php echo $home_page; ?>" target="_blank">Link</a></td>
+							<td><?php echo $tipo_de_instituicao; ?></td>
+						</tr>
+						<?php
+					endwhile;
+					?>
+				</tbody>
+			</table>
+		</div>
 	</div>
 </main>
 <?php get_footer(); ?>
