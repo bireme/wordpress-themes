27fa48a585ff36d93307ecb23a050cd0427755d8
Márcio Alves <marcioalvessilvajc@gmail.com>
2021-08-25 10:56:43 -0300

    Loops de sessões na homepage - Open Metadada

--- openmetadata/home.php
+++ openmetadata/home.php
@@ -5,8 +5,7 @@ Template Name: Home
 ?>
 <?php get_header(); ?>
 <?php get_template_part('includes/nav') ?>
-<?php
-$home = new WP_Query([ 'post_type' => 'Home']);
+<?php $home = new WP_Query([ 'post_type' => 'Home']);
 while($home->have_posts()):$home->the_post();
 	while(have_rows('group_1')):the_row(); 
 		$title = get_sub_field('title'); 
@@ -30,58 +29,114 @@ while($home->have_posts()):$home->the_post();
 endwhile;
 ?>
 
+
 <?php
-while($home->have_posts()):$home->the_post();
-	while(have_rows('group_2')):the_row(); 
-		$title = get_sub_field('title'); 
-		$text = get_sub_field('text'); 
-		?>
-		<section class="padding2 color1" id="howtoUse">
-			<div class="container">
-				<h1 class="title1"><?php echo $title ?></h1>
-				<?php echo $text ?>
-			</div>
-		</section>
-		<?php
-	endwhile;
+while(have_rows('group_2')):the_row(); 
+	$title = get_sub_field('title'); 
+	$text = get_sub_field('text'); 
+	?>
+	<section class="padding2 color1" id="howtoUse">
+		<div class="container">
+			<h1 class="title1"><?php echo $title ?></h1>
+			<?php echo $text ?>
+		</div>
+	</section>
+	<?php
 endwhile;
 ?>
 
 
-<?php 
-while($home->have_posts()):$home->the_post();
-	if( $home->have_rows('group_faq') ): while ( $home->have_rows('group_faq') ) : the_row(); $title = get_sub_field('text_1'); endwhile; endif; ?>
-	<section class="padding2 <?php echo $title==''?'d-none':''; ?>">
-		<div class="container">
-			<h1 class="title1">FAQ</h1>
-			<div class="accordion" id="accordionExample">
-				<?php if( have_rows('group_faq') ): ?>
-					<?php while( have_rows('group_faq') ): the_row(); $row = get_row(); $count = count($row); $loop = 0; ?>
-						<?php while ($count > $loop) : $loop++; ?>
-							<?php
-							$title = get_sub_field('title_'.$loop);
-							$text = get_sub_field('text_'.$loop);
-							?>
-							<?php if ( $text ) : ?>
 
-								<div class="accordion-item">
-									<h2 class="accordion-header" id="heading_<?= $loop ?>">
-										<button class="accordion-button <?=$loop=='1'?'':'collapsed'; ?>" type="button" data-bs-toggle="collapse" data-bs-target="#collapse_<?= $loop ?>" aria-expanded="<?=$loop=='1'?'true':'false'; ?>" aria-controls="collapse_<?= $loop ?>">
-											<?php echo $title;  ?>
-										</button>
-									</h2>
-									<div id="collapse_<?= $loop ?>" class="accordion-collapse collapse <?=$loop=='1'?'show':''; ?>" aria-labelledby="heading_<?= $loop ?>" data-bs-parent="#accordionExample">
-										<div class="accordion-body">
-											<?php echo $text;  ?>
-										</div>
+<?php while ( $home->have_rows('group_faq') ) : the_row(); $title = get_sub_field('text_1'); endwhile; ?>
+<section class="padding2 <?php echo $title==''?'d-none':''; ?>">
+	<div class="container">
+		<h1 class="title1">FAQ</h1>
+		<div class="accordion" id="accordionExample">
+			<?php if( have_rows('group_faq') ): ?>
+				<?php while( have_rows('group_faq') ): the_row(); $row = get_row(); $count = count($row); $loop = 0; ?>
+					<?php while ($count > $loop) : $loop++; ?>
+						<?php
+						$title = get_sub_field('title_'.$loop);
+						$text = get_sub_field('text_'.$loop);
+						?>
+						<?php if ( $text ) : ?>
+							<div class="accordion-item">
+								<h2 class="accordion-header" id="heading_<?= $loop ?>">
+									<button class="accordion-button <?=$loop=='1'?'':'collapsed'; ?>" type="button" data-bs-toggle="collapse" data-bs-target="#collapse_<?= $loop ?>" aria-expanded="<?=$loop=='1'?'true':'false'; ?>" aria-controls="collapse_<?= $loop ?>">
+										<?php echo $title;  ?>
+									</button>
+								</h2>
+								<div id="collapse_<?= $loop ?>" class="accordion-collapse collapse <?=$loop=='1'?'show':''; ?>" aria-labelledby="heading_<?= $loop ?>" data-bs-parent="#accordionExample">
+									<div class="accordion-body">
+										<?php echo $text;  ?>
 									</div>
 								</div>
-							<?php endif; ?>
-						<?php endwhile; ?>
+							</div>
+						<?php endif; ?>
 					<?php endwhile; ?>
-				<?php endif; ?>
-			<?php endwhile; ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
 		</div>
 	</div>
 </section>
+
+
+
+
+<?php while ( $home->have_rows('new_section') ) : the_row(); $title = get_sub_field('text_1'); endwhile; ?>
+<section class="padding2 <?php echo $title==''?'d-none':''; ?>">
+	<div class="container">
+		<h1 class="title1">FAQ</h1>
+		<div class="accordion" id="accordionExample">
+			<?php if( have_rows('new_section') ): ?>
+				<?php while( have_rows('new_section') ): the_row(); $row = get_row(); $count = count($row); $loop = 0; ?>
+					<?php while ($count > $loop) : $loop++; ?>
+						<?php
+						$title = get_sub_field('title_'.$loop);
+						$text = get_sub_field('text_'.$loop);
+						?>
+						<?php if ( $text ) : ?>
+							<div class="accordion-item">
+								<h2 class="accordion-header" id="heading_<?= $loop ?>">
+									<button class="accordion-button <?=$loop=='1'?'':'collapsed'; ?>" type="button" data-bs-toggle="collapse" data-bs-target="#collapse_<?= $loop ?>" aria-expanded="<?=$loop=='1'?'true':'false'; ?>" aria-controls="collapse_<?= $loop ?>">
+										<?php echo $title;  ?>
+									</button>
+								</h2>
+								<div id="collapse_<?= $loop ?>" class="accordion-collapse collapse <?=$loop=='1'?'show':''; ?>" aria-labelledby="heading_<?= $loop ?>" data-bs-parent="#accordionExample">
+									<div class="accordion-body">
+										<?php echo $text;  ?>
+									</div>
+								</div>
+							</div>
+						<?php endif; ?>
+					<?php endwhile; ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+	</div>
+</section>
+
+
+<?php while($home->have_rows('new_section')) : the_row(); $title = get_sub_field('text_1'); endwhile; ?>
+<?php if( have_rows('new_section') ): ?>
+	<?php while( have_rows('new_section') ): the_row(); $row = get_row(); $count = count($row); $loop = 0; ?>
+		<?php while ($count > $loop) : $loop++; ?>
+			<?php
+			$title = get_sub_field('title_'.$loop);
+			$text = get_sub_field('text_'.$loop);
+			?>
+			<section class="padding2 sectionColor <?php echo $title==''?'d-none':''; ?>">
+				<div class="container">
+					<?php if ($text):?>
+						<h1 class="title1"><?php echo $title; ?></h1>
+						<?php echo $text;  ?>
+					<?php endif; ?>
+				</div>
+			</section>
+		<?php endwhile; ?>
+	<?php endwhile; ?>
+<?php endif; ?>
+
+
+
 <?php get_footer(); ?>
--- openmetadata/index.php
+++ openmetadata/index.php
@@ -1,95 +1,137 @@
 <?php get_header(); ?>
 <?php get_template_part('includes/nav') ?>
-<section class="padding2" id="main_container">
-		<div class="container">
-			<h1 class="title1">Por que usar?</h1>
-			<div class="row">
-				<div class="col-md-8">
-					<p>Lorem, ipsum dolor sit amet consectetur adipisicing, elit. Rerum expedita, esse! Veritatis, excepturi deserunt quasi consequatur quo, repudiandae, vitae magni cumque voluptatum harum aliquam soluta quidem qui, id recusandae eveniet et accusantium. Facere vero hic impedit placeat at, voluptates quas neque veritatis quos tempore eos architecto, ullam assumenda quibusdam porro error, perspiciatis sit vitae! Fugit aut voluptatum voluptatem, autem rerum, expedita explicabo hic tempore dignissimos quas tenetur, culpa dolor. Libero alias, nobis eum maiores itaque rerum numquam consectetur unde officiis beatae odit eos vel perspiciatis veniam similique odio, reiciendis. Recusandae excepturi aliquam odio, dicta earum laboriosam quaerat quis autem atque.</p>
-				</div>
-				<div class="col-md-4">
-					<img src="<?php bloginfo('template_directory') ?>/img/search.png" class="img-fluid" alt="">
+<?php $home = new WP_Query([ 'post_type' => 'Home']);
+while($home->have_posts()):$home->the_post();
+	while(have_rows('group_1')):the_row(); 
+		$title = get_sub_field('title'); 
+		$text = get_sub_field('text'); 
+		?>
+		<section class="padding2" id="main_container">
+			<div class="container">
+				<h1 class="title1"><?php echo $title ?></h1>
+				<div class="row">
+					<div class="col-md-8">
+						<?php echo $text ?>
+					</div>
+					<div class="col-md-4">
+						<img src="<?php bloginfo('template_directory') ?>/img/search.png" class="img-fluid" alt="">
+					</div>
 				</div>
 			</div>
-		</div>
-	</section>
+		</section>
+		<?php
+	endwhile;
+endwhile;
+?>
 
+
+<?php
+while(have_rows('group_2')):the_row(); 
+	$title = get_sub_field('title'); 
+	$text = get_sub_field('text'); 
+	?>
 	<section class="padding2 color1" id="howtoUse">
 		<div class="container">
-			<h1 class="title1">Como usar</h1>
-			<p>Lorem, ipsum dolor sit amet consectetur adipisicing, elit. Rerum expedita, esse! Veritatis, excepturi deserunt quasi consequatur quo, repudiandae, vitae magni cumque voluptatum harum aliquam soluta quidem qui, id recusandae eveniet et accusantium. Facere vero hic impedit placeat at, voluptates quas neque veritatis quos tempore eos architecto, ullam assumenda quibusdam porro error, perspiciatis sit vitae! Fugit aut voluptatum voluptatem, autem rerum, expedita explicabo hic tempore dignissimos quas tenetur, culpa dolor. Libero alias, nobis eum maiores itaque rerum numquam consectetur unde officiis beatae odit eos vel perspiciatis veniam similique odio, reiciendis. Recusandae excepturi aliquam odio, dicta earum laboriosam quaerat quis autem atque.</p>
-			<a href="como-usar.php" class="btn btn-outline-warning">Mais detalhes de como usar</a>
+			<h1 class="title1"><?php echo $title ?></h1>
+			<?php echo $text ?>
 		</div>
 	</section>
+	<?php
+endwhile;
+?>
 
 
 
-
-	<section class="padding2">
-		<div class="container">
-			<h1 class="title1">FAQ</h1>
-			<div class="accordion" id="accordionExample">
-				<div class="accordion-item">
-					<h2 class="accordion-header" id="headingOne">
-						<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-							Quais são os formatos disponíveis?
-						</button>
-					</h2>
-					<div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-						<div class="accordion-body">
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime repellat necessitatibus nesciunt, doloribus aliquid distinctio hic quisquam ducimus autem excepturi? Nam, saepe hic fugit, laudantium consequatur laboriosam corporis omnis iusto.
-						</div>
-					</div>
-				</div>
-				<div class="accordion-item">
-					<h2 class="accordion-header" id="headingTwo">
-						<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-							Preciso divulgar a marca BVS em meu portal?
-						</button>
-					</h2>
-					<div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-						<div class="accordion-body">
-							Lorem ipsum dolor sit amet consectetur adipisicing, elit. Nostrum, similique fugit, amet sit, illo aperiam facere explicabo, quis quod ducimus voluptas incidunt dolores ullam natus error accusamus debitis? Libero cum non dignissimos nam blanditiis earum, est ab excepturi accusantium, dolores at fugiat saepe porro minus nobis. Repellendus culpa tempore temporibus earum quia, accusamus, sequi amet, recusandae dolorem non qui exercitationem voluptas, repellat expedita vero. Placeat sint deserunt beatae recusandae hic minima amet vitae veritatis exercitationem quidem ad necessitatibus quae, unde laborum quam nobis consequatur culpa, natus quas voluptas ea nulla a doloremque. Dignissimos quis sed earum temporibus quam hic explicabo!
-						</div>
-					</div>
-				</div>
-				<div class="accordion-item">
-					<h2 class="accordion-header" id="headingThree">
-						<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-							Preciso de licença para usar os dados capturados?
-						</button>
-					</h2>
-					<div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-						<div class="accordion-body">
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, ut, sequi repudiandae amet atque unde reiciendis dicta optio placeat blanditiis id, saepe. Dolores labore ex eligendi facere voluptate expedita repudiandae.
-						</div>
-					</div>
-				</div>
-				<div class="accordion-item">
-					<h2 class="accordion-header" id="headingFour">
-						<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, facere?
-						</button>
-					</h2>
-					<div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
-						<div class="accordion-body">
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, ut, sequi repudiandae amet atque unde reiciendis dicta optio placeat blanditiis id, saepe. Dolores labore ex eligendi facere voluptate expedita repudiandae.
-						</div>
-					</div>
-				</div>
-				<div class="accordion-item">
-					<h2 class="accordion-header" id="headingFive">
-						<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-							Lorem ipsum dolor sit amet consectetur adipisicing, elit. Omnis, optio. Sapiente nemo aliquam excepturi cupiditate soluta repudiandae necessitatibus corrupti aut?
-						</button>
-					</h2>
-					<div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
-						<div class="accordion-body">
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, ut, sequi repudiandae amet atque unde reiciendis dicta optio placeat blanditiis id, saepe. Dolores labore ex eligendi facere voluptate expedita repudiandae.
-						</div>
-					</div>
-				</div>
-			</div>
+<?php while ( $home->have_rows('group_faq') ) : the_row(); $title = get_sub_field('text_1'); endwhile; ?>
+<section class="padding2 <?php echo $title==''?'d-none':''; ?>">
+	<div class="container">
+		<h1 class="title1">FAQ</h1>
+		<div class="accordion" id="accordionExample">
+			<?php if( have_rows('group_faq') ): ?>
+				<?php while( have_rows('group_faq') ): the_row(); $row = get_row(); $count = count($row); $loop = 0; ?>
+					<?php while ($count > $loop) : $loop++; ?>
+						<?php
+						$title = get_sub_field('title_'.$loop);
+						$text = get_sub_field('text_'.$loop);
+						?>
+						<?php if ( $text ) : ?>
+							<div class="accordion-item">
+								<h2 class="accordion-header" id="heading_<?= $loop ?>">
+									<button class="accordion-button <?=$loop=='1'?'':'collapsed'; ?>" type="button" data-bs-toggle="collapse" data-bs-target="#collapse_<?= $loop ?>" aria-expanded="<?=$loop=='1'?'true':'false'; ?>" aria-controls="collapse_<?= $loop ?>">
+										<?php echo $title;  ?>
+									</button>
+								</h2>
+								<div id="collapse_<?= $loop ?>" class="accordion-collapse collapse <?=$loop=='1'?'show':''; ?>" aria-labelledby="heading_<?= $loop ?>" data-bs-parent="#accordionExample">
+									<div class="accordion-body">
+										<?php echo $text;  ?>
+									</div>
+								</div>
+							</div>
+						<?php endif; ?>
+					<?php endwhile; ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
 		</div>
-	</section>
+	</div>
+</section>
+
+
+
+
+<?php while ( $home->have_rows('new_section') ) : the_row(); $title = get_sub_field('text_1'); endwhile; ?>
+<section class="padding2 <?php echo $title==''?'d-none':''; ?>">
+	<div class="container">
+		<h1 class="title1">FAQ</h1>
+		<div class="accordion" id="accordionExample">
+			<?php if( have_rows('new_section') ): ?>
+				<?php while( have_rows('new_section') ): the_row(); $row = get_row(); $count = count($row); $loop = 0; ?>
+					<?php while ($count > $loop) : $loop++; ?>
+						<?php
+						$title = get_sub_field('title_'.$loop);
+						$text = get_sub_field('text_'.$loop);
+						?>
+						<?php if ( $text ) : ?>
+							<div class="accordion-item">
+								<h2 class="accordion-header" id="heading_<?= $loop ?>">
+									<button class="accordion-button <?=$loop=='1'?'':'collapsed'; ?>" type="button" data-bs-toggle="collapse" data-bs-target="#collapse_<?= $loop ?>" aria-expanded="<?=$loop=='1'?'true':'false'; ?>" aria-controls="collapse_<?= $loop ?>">
+										<?php echo $title;  ?>
+									</button>
+								</h2>
+								<div id="collapse_<?= $loop ?>" class="accordion-collapse collapse <?=$loop=='1'?'show':''; ?>" aria-labelledby="heading_<?= $loop ?>" data-bs-parent="#accordionExample">
+									<div class="accordion-body">
+										<?php echo $text;  ?>
+									</div>
+								</div>
+							</div>
+						<?php endif; ?>
+					<?php endwhile; ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+	</div>
+</section>
+
+
+<?php while($home->have_rows('new_section')) : the_row(); $title = get_sub_field('text_1'); endwhile; ?>
+<?php if( have_rows('new_section') ): ?>
+	<?php while( have_rows('new_section') ): the_row(); $row = get_row(); $count = count($row); $loop = 0; ?>
+		<?php while ($count > $loop) : $loop++; ?>
+			<?php
+			$title = get_sub_field('title_'.$loop);
+			$text = get_sub_field('text_'.$loop);
+			?>
+			<section class="padding2 sectionColor <?php echo $title==''?'d-none':''; ?>">
+				<div class="container">
+					<?php if ($text):?>
+						<h1 class="title1"><?php echo $title; ?></h1>
+						<?php echo $text;  ?>
+					<?php endif; ?>
+				</div>
+			</section>
+		<?php endwhile; ?>
+	<?php endwhile; ?>
+<?php endif; ?>
+
+
+
 <?php get_footer(); ?>
