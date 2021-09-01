8ad1f118aa8eeed506620ab6831b4420c8329159
Márcio Alves <marcioalvessilvajc@gmail.com>
2021-09-01 15:19:47 -0300

    Ajuste tabela página de instituições - BiblioSUS

--- bibliosus/instituicoes.php
+++ bibliosus/instituicoes.php
@@ -8,62 +8,57 @@
 		<p><?php the_content(); ?></p>
 		<br><hr><br>
 		<input class="form-control" id="myInput" type="text" placeholder="Pesquisar..."> <br>
-		<div class="table-responsive">
-			<table class="table table-bordered table-striped">
-				<thead>
-					<tr>
-						<th>Nome da Instituição</th>
-						<th>CNPJ</th>
-						<th>Contato</th>
-						<th>Endereço</th>
-						<th>Página Web</th>
-						<th>Tipo</th>
-					</tr>
-				</thead>
-				<tbody  id="myTable">
-					<?php 
-					$posts = new WP_Query([
-						'post_type' => 'instituicoes',
-						'orderby' => 'title',
-						'order' => 'ASC',
-						'posts_per_page' => '-1'
-					]);
-					while($posts->have_posts()):$posts->the_post();
-						$nome_da_unidade 		= get_field('$nome_da_unidade'); 
-						$cnpj 					= get_field('cnpj'); 
-						$endereco_completo 		= get_field('endereco_completo'); 
-						$cidade 				= get_field('cidade'); 
-						$uf 					= get_field('uf'); 
-						$cep 					= get_field('cep'); 
-						$telefones 				= get_field('telefones_-_ramal'); 
-						$email 					= get_field('e-mail'); 
-						$home_page 				= get_field('home_page'); 
-						$tipo_de_instituicao 	= get_field('tipo_de_instituicao');
-						?>
-						<tr>
-							<td><b><?php the_title(); ?></b></td>
-							<td><?=$cnpj; ?></td>
-							<td><i class="fas fa-phone-alt"></i> <?=$telefones; ?> <br> <i class="far fa-envelope"></i> <?=$email; ?></td>
-							<td><i class="fas fa-map-marker-alt"></i> <?=$endereco_completo; ?> <br> <?=$cidade; ?> - <?=$uf; ?> - <?=$cep; ?></td>
-							<td><?php echo $home_page !== ""? "<a href='http://$home_page' class='btn btn-sm btn-outline-success'>Link</a>" : ""; ?></td>
-							<td><?=$tipo_de_instituicao; ?></td>
-						</tr>
-						<?php
-					endwhile;
+		<table class="table table-bordered table-striped">
+			<thead>
+				<tr>
+					<th>Nome da Instituição</th>
+					<th>Contato</th>
+					<th>Endereço</th>
+					<th>Página Web</th>
+				</tr>
+			</thead>
+			<tbody  id="myTable">
+				<?php 
+				$posts = new WP_Query([
+					'post_type' => 'instituicoes',
+					'orderby' => 'title',
+					'order' => 'ASC',
+					'posts_per_page' => '-1'
+				]);
+				while($posts->have_posts()):$posts->the_post();
+					$nome_da_unidade 		= get_field('$nome_da_unidade'); 
+					$cnpj 					= get_field('cnpj'); 
+					$endereco_completo 		= get_field('endereco_completo'); 
+					$cidade 				= get_field('cidade'); 
+					$uf 					= get_field('uf'); 
+					$cep 					= get_field('cep'); 
+					$telefones 				= get_field('telefones_-_ramal'); 
+					$email 					= get_field('e-mail'); 
+					$home_page 				= get_field('home_page'); 
+					$tipo_de_instituicao 	= get_field('tipo_de_instituicao');
 					?>
-				</tbody>
-			</table>
-			<script>
-				$(document).ready(function(){
-					$("#myInput").on("keyup", function() {
-						var value = $(this).val().toLowerCase();
-						$("#myTable tr").filter(function() {
-							$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-						});
+					<tr>
+						<td><b><?php the_title(); ?></b> <br> <small><?=$cnpj; ?> - <?=$tipo_de_instituicao; ?></small></td>
+						<td><i class="fas fa-phone-alt"></i> <?=$telefones; ?> <br> <i class="far fa-envelope"></i> <?=$email; ?></td>
+						<td><i class="fas fa-map-marker-alt"></i> <?=$endereco_completo; ?> <br> <?=$cidade; ?> - <?=$uf; ?> - <?=$cep; ?></td>
+						<!-- <td><?php echo $home_page !== ""? "<a href='http://$home_page' class='btn btn-sm btn-outline-success' target='_blank'>Link</a>" : ""; ?></td> -->
+						<td><?php echo $home_page; ?></td>
+					</tr>
+					<?php
+				endwhile;
+				?>
+			</tbody>
+		</table>
+		<script>
+			$(document).ready(function(){
+				$("#myInput").on("keyup", function() {
+					var value = $(this).val().toLowerCase();
+					$("#myTable tr").filter(function() {
+						$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
 					});
 				});
-			</script>
-		</div>
+			});
+		</script>
 	</div>
 </main>
 <?php get_footer(); ?>
