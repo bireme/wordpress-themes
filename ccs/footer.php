415a18aaf399f13c567fa90cb8a5c647d4c5c5ee
Márcio Alves <marcioalvessilvajc@gmail.com>
2022-01-05 09:30:53 -0300

    Ajuste Comunicação Científica em Ciências da Saúde

--- ccs/footer.php
+++ ccs/footer.php
@@ -4,15 +4,15 @@
 			<div class="row">
 				<div class="col-md-5">
 					<b><?php pll_e('Comunicação Científica em Saúde'); ?></b> <br>
-					<a href="http://politicas.bireme.org/terminos/<?php echo $language==''?'es':$language; ?>/" target="_blank">
+					<a href="http://politicas.bireme.org/terminos/<?php echo $language==''?'pt':$language; ?>/" target="_blank">
 						<?php pll_e('Terms and conditions of use'); ?>
 					</a> | 
-					<a href="http://politicas.bireme.org/privacidad/<?php echo $language==''?'es':$language; ?>/" target="_blank">
+					<a href="http://politicas.bireme.org/privacidad/<?php echo $language==''?'pt':$language; ?>/" target="_blank">
 						<?php pll_e('Privacy policy'); ?>
 					</a>
 				</div>
 				<div class="col-md-7 text-end">
-					<img src="http://logos.bireme.org/img/pt/h_bir_white.svg" alt="" class="img-fluid">
+					<img src="http://logos.bireme.org/img/<?php echo $language==''?'pt':$language; ?>/h_bir_white.svg" alt="" class="img-fluid">
 				</div>
 			</div>
 		</div>
--- ccs/header.php
+++ ccs/header.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html <?php language_attributes() ?> >
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<?php wp_head(); ?>
 </head>
 <body>
+	<?php $language = pll_current_language(); ?>
 	<?php get_template_part('includes/topAccessibility') ?>
 	<header id="header">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-8">
 					<div class="float-start">
-						<img src="http://logos.bireme.org/img/pt/bvs_color.svg" alt="" class="img-fluid" style="width: 90px; margin-right: 10px; background: #fff;">
+						<img src="http://logos.bireme.org/img/<?php echo $idioma; ?>/bvs_color.svg" alt="Logo BVS" class="img-fluid" style="width: 90px; margin-right: 10px; background: #fff;">
 					</div>
 					<h1><?php bloginfo('name');?></h1>
 				</div>
