a8e09a8bdc9a7479c32a1ea06e1f9cd7297d02fb
Márcio Alves <marcioalvessilvajc@gmail.com>
2022-02-10 08:46:06 -0300

    Include área de busca - página BRISA

---
+++ redetsa/includes/search-brisa.php
@@ -0,0 +1,18 @@
+<section id="searchBrisa" class="padding2">
+    <div class="container">
+      <form id="formHome" method="get" action="">
+        <div class="row g-3">
+          <div class="col-9 offset-1" style="position:relative;">
+            <input type="text" id="fieldSearch" class="form-control" autocomplete="off" placeholder="Buscar">
+            <input type="hidden" name="post_type" value="post">
+            <a id="speakBtn" href="#"><i class="fas fa-microphone-alt"></i></a>
+          </div>
+          <div class="col-1 float-end">
+            <button type="submit" id="submitHome" class="btn btn-primary">
+              <i class="fas fa-search"></i>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </section>
