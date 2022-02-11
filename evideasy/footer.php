36d85aaee3fa423bf31648b34c440839e7590a3e
Márcio Alves <marcioalvessilvajc@gmail.com>
2022-02-11 17:06:31 -0300

    Ajuste no footer do tema Evid@Easy

--- evideasy/footer.php
+++ evideasy/footer.php
@@ -4,12 +4,12 @@
 			<div class="row">
 				<div class="col-md-6">
 					<b>EVID@Easy</b> <br>
-					<a href="http://politicas.bireme.org/terminos/<?php echo $idioma?>/" target="_blank"><?php pll_e('Terms and conditions of use'); ?></a> | 
-					<a href="http://politicas.bireme.org/privacidad/<?php echo $idioma?>/" target="_blank"><?php pll_e('Privacy Policy'); ?></a>
+					<a href="http://politicas.bireme.org/terminos/<?php echo $idioma=='fr'?'en':$idioma; ?>/" target="_blank"><?php pll_e('Terms and conditions of use'); ?></a> | 
+					<a href="http://politicas.bireme.org/privacidad/<?php echo $idioma=='fr'?'en':$idioma; ?>/" target="_blank"><?php pll_e('Privacy Policy'); ?></a>
 				</div>
 				<div class="col-md-6 text-right" id="logoBir">
 					<a href="https://www.paho.org/<?php echo $idioma?>/bireme" target="_blank">
-						<img src="http://logos.bireme.org/img/<?php echo $idioma; ?>/v_bir_color.svg" alt="" class="imgBlack">
+						<img src="http://logos.bireme.org/img/<?php echo $idioma=='fr'?'en':$idioma; ?>/v_bir_color.svg" alt="" class="imgBlack">
 					</a>
 				</div>
 			</div>
