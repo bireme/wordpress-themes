4d98639889834d0f9abe034ab830bde07cec7a10
wilsonmoura <wilsonsmoura@gmail.com>
2022-03-17 19:06:13 -0300

    Fixed #101 -- Tema RedETSA: Desenvolver menu para CPT de eventos

--- redetsa/eventos.php
+++ redetsa/eventos.php
@@ -3,7 +3,18 @@
 Template Name: Eventos
 ***/
 ?>
-<?php get_header();?>
+<?php
+    $count = 0;
+
+    $args = array(
+        'post_type' => 'eventos',
+        'post_status' => 'publish',
+        'posts_per_page' => -1
+    );
+
+    $events = new WP_Query( $args );
+?>
+<?php get_header(); ?>
 <?php get_template_part('includes/nav'); ?>
 <main id="main_container" class="padding1">
   <div class="container">
@@ -12,65 +23,51 @@ Template Name: Eventos
         <h1 class="title1"><?php the_title(); ?></h1>
         <?php while(have_posts()) : the_post(); ?>
           <?php the_post_thumbnail('large',['class' => 'img-fluid']); ?>
-          <?php the_content();
-        endwhile;
-        ?>
+          <?php the_content(); ?>
+        <?php endwhile; ?>
       </div>
-
-      
       <div class="col-md-4 col-lg-3 order-md-first">
         <div class="accordion" id="accordionExample">
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="headingOne">
-              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                XII Encuentro Anual 2021
-              </button>
-            </h2>
-            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                <ul>
-                  <li><a href="#sessao1">Agenda</a></li>
-                  <li><a href="#sessao2">Fotos</a></li>
-                  <li><a href="#sessao3">Participações</a></li>
-                </ul>
+          <?php if ($events->have_posts()): while ($events->have_posts()) : $events->the_post(); $count++; ?>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="heading<?php echo $count; ?>">
+                <button class="accordion-button <?php if ( $count > 1 ) { echo 'collapsed'; } ?>" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $count; ?>" aria-expanded="<?php echo ( $count == 1 ) ? 'true' : 'false'; ?>" aria-controls="collapse<?php echo $count; ?>">
+                  <?php the_title(); ?>
+                </button>
+              </h2>
+              <div id="collapse<?php echo $count; ?>" class="accordion-collapse collapse <?php if ( $count == 1 ) { echo 'show'; } ?>" aria-labelledby="heading<?php echo $count; ?>" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <ul>
+                    <?php
+                      $section  = 0;
+                      $first    = true;
+
+                      $groups   = array();
+                      $groups[] = get_field('grupo_1');
+                      $groups[] = get_field('grupo_2');
+                      $groups[] = get_field('grupo_3');
+                    ?>
+                    <?php foreach ($groups as $group) : $section++; ?>
+                      <?php if ( $group['title'] ) : ?>
+                        <?php
+                          if ( $first ) {
+                            $hash = '';
+                            $first = false;
+                          } else {
+                            $hash = '#section' . $section;
+                          }
+                        ?>
+                        <li><a href="<?php echo get_the_permalink() . $hash; ?>"><?php echo $group['title']; ?></a></li>
+                      <?php endif; ?>
+                    <?php endforeach; ?>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="headingTwo">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                XI Encuentro Anual 2020
-              </button>
-            </h2>
-            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                <ul>
-                  <li><a href="#sessao1">Agenda</a></li>
-                  <li><a href="#sessao2">Fotos</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="headingThree">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                X Encuentro Anual 2019
-              </button>
-            </h2>
-            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                <ul>
-                  <li><a href="#sessao1">Agenda</a></li>
-                  <li><a href="#sessao2">Fotos</a></li>
-                  <li><a href="#sessao3">Videos</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
+          <?php endwhile; endif; ?>
         </div>
       </div>
     </div>
-    
   </div>
 </main>
 <?php get_footer(); ?>
--- redetsa/single-eventos.php
+++ redetsa/single-eventos.php
@@ -1,4 +1,16 @@
-<?php get_header();?>
+<?php
+    $count = 0;
+    $post_id = get_the_ID();
+
+    $args = array(
+        'post_type' => 'eventos',
+        'post_status' => 'publish',
+        'posts_per_page' => -1
+    );
+
+    $events = new WP_Query( $args );
+?>
+<?php get_header(); ?>
 <?php get_template_part('includes/nav'); ?>
 <main id="main_container" class="padding1">
   <div class="container">
@@ -7,104 +19,96 @@
         <h1 class="title1"><?php the_title(); ?></h1>
         <?php while(have_posts()) : the_post(); ?>
           <?php the_post_thumbnail('large',['class' => 'img-fluid']); ?>
-          <?php the_content();
-        endwhile;
-        ?>
+          <?php the_content(); ?>
+        <?php endwhile; ?>
 
         <?php if( have_rows('grupo_1') ): ?>
           <?php while( have_rows('grupo_1') ): the_row(); 
             $title = get_sub_field('title');
             $contents = get_sub_field('contents');
-            ?>
-            <div id="sessao1">
-              <div class="content">
-                <h2 class="title1"><?php echo $title; ?></h2>
-                <?php echo $contents; ?>
+          ?>
+            <?php if ( $title ) : ?>
+              <div id="section1">
+                <div class="content">
+                  <h2 class="title1"><?php echo $title; ?></h2>
+                  <?php echo $contents; ?>
+                </div>
               </div>
-            </div>
+            <?php endif; ?>
           <?php endwhile; ?>
         <?php endif; ?>
-
 
         <?php if( have_rows('grupo_2') ): ?>
           <?php while( have_rows('grupo_2') ): the_row(); 
             $title = get_sub_field('title');
             $contents = get_sub_field('contents');
-            ?>
-            <div id="sessao2">
-              <div class="content">
-                <h2 class="title1"><?php echo $title; ?></h2>
-                <?php echo $contents; ?>
+          ?>
+            <?php if ( $title ) : ?>
+              <div id="section2">
+                <div class="content">
+                  <h2 class="title1"><?php echo $title; ?></h2>
+                  <?php echo $contents; ?>
+                </div>
               </div>
-            </div>
+            <?php endif; ?>
           <?php endwhile; ?>
         <?php endif; ?>
-
 
         <?php if( have_rows('grupo_3') ): ?>
           <?php while( have_rows('grupo_3') ): the_row(); 
             $title = get_sub_field('title');
             $contents = get_sub_field('contents');
-            ?>
-            <div id="sessao3">
-              <div class="content">
-                <h2 class="title1"><?php echo $title; ?></h2>
-                <?php echo $contents; ?>
+          ?>
+            <?php if ( $title ) : ?>
+              <div id="section3">
+                <div class="content">
+                  <h2 class="title1"><?php echo $title; ?></h2>
+                  <?php echo $contents; ?>
+                </div>
               </div>
-            </div>
+            <?php endif; ?>
           <?php endwhile; ?>
         <?php endif; ?>
-
       </div>
       <div class="col-md-4 col-lg-3 order-md-first">
-        <div class="accordion sticky-top" id="accordionExample">
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="headingOne">
-              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                XII Encuentro Anual 2021
-              </button>
-            </h2>
-            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                <ul>
-                  <li><a href="#sessao1">Agenda</a></li>
-                  <li><a href="#sessao2">Fotos</a></li>
-                  <li><a href="#sessao3">Participações</a></li>
-                </ul>
+        <div class="accordion" id="accordionExample">
+          <?php if ($events->have_posts()): while ($events->have_posts()) : $events->the_post(); $count++; ?>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="heading<?php echo $count; ?>">
+                <button class="accordion-button <?php if ( $post_id != get_the_ID() ) { echo 'collapsed'; } ?>" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $count; ?>" aria-expanded="<?php echo ( $post_id == get_the_ID() ) ? 'true' : 'false'; ?>" aria-controls="collapse<?php echo $count; ?>">
+                  <?php the_title(); ?>
+                </button>
+              </h2>
+              <div id="collapse<?php echo $count; ?>" class="accordion-collapse collapse <?php if ( $post_id == get_the_ID() ) { echo 'show'; } ?>" aria-labelledby="heading<?php echo $count; ?>" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <ul>
+                    <?php
+                      $section  = 0;
+                      $first    = true;
+
+                      $groups   = array();
+                      $groups[] = get_field('grupo_1');
+                      $groups[] = get_field('grupo_2');
+                      $groups[] = get_field('grupo_3');
+                    ?>
+                    <?php foreach ($groups as $group) : $section++; ?>
+                      <?php if ( $group['title'] ) : ?>
+                        <?php
+                          if ( $first && $post_id != get_the_ID() ) {
+                            $hash = '';
+                            $first = false;
+                          } else {
+                            $hash = '#section' . $section;
+                          }
+                        ?>
+                        <li><a href="<?php echo get_the_permalink() . $hash; ?>"><?php echo $group['title']; ?></a></li>
+                      <?php endif; ?>
+                    <?php endforeach; ?>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="headingTwo">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                XI Encuentro Anual 2020
-              </button>
-            </h2>
-            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                <ul>
-                  <li><a href="#sessao1">Agenda</a></li>
-                  <li><a href="#sessao2">Fotos</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="headingThree">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                X Encuentro Anual 2019
-              </button>
-            </h2>
-            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                <ul>
-                  <li><a href="#sessao1">Agenda</a></li>
-                  <li><a href="#sessao2">Fotos</a></li>
-                  <li><a href="#sessao3">Videos</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
+          <?php endwhile; endif; ?>
         </div>
       </div>
     </div>
