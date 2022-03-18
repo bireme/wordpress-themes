9eed64267700adf90f4e4e064872407b02e668b0
Márcio Alves <marcioalvessilvajc@gmail.com>
2022-03-18 15:24:06 -0300

    Imagem para sessão de perfil de paises - RedETSA

--- redetsa/home.php
+++ redetsa/home.php
@@ -27,7 +27,7 @@ endwhile;
 
 
 
-<section class="padding1" id="bg-perfil">>
+<section class="padding1" id="bg-perfil">
   <div class="container">
     <div class="row row-cols-1 row-cols-md-3 g-4">
       <?php
