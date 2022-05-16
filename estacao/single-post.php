19adb4b83e6d7d3aa7e90b3b567fd033ab6016eb
Márcio Alves <marcioalvessilvajc@gmail.com>
2022-05-16 15:15:11 -0300

    Ajsutes tema Estação BVS

--- estacao/single-post.php
+++ estacao/single-post.php
@@ -3,6 +3,7 @@
 <main id="main_container" class="padding1">
 	<div class="container">
 		<h1 class=" title1"><?php the_title(); ?></h1><br> 
+		<div class="text-center"><?php if ( has_post_thumbnail()) { the_post_thumbnail('large', ['class' => 'img-fluid margin1']);} ?></div>
 		<?php while(have_posts()) : the_post();?>
 			<?php the_content(); ?>
 		<?php	endwhile;	?>
