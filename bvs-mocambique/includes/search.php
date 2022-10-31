8e99d8f374c9d534dc60e5e5727e41ae2ee50e99
Márcio Alves <marcioalvessilvajc@gmail.com>
2022-10-31 14:55:00 -0300

    Ajuste BVS Moçambique - Pesquisa

--- bvs-mocambique/includes/search.php
+++ bvs-mocambique/includes/search.php
@@ -1,13 +1,13 @@
 <section id="sectionSearch" class="padding1">
 	<div class="container">
 
-		<form id="formHome" method="get"  action="https://pesquisa.bvsalud.org" >
+		<form id="formHome" method="get"  action="https://mocambique.teste.eportuguese.org/biblio" >
 			<div class="row g-3">
 				<div class="col-9 offset-1" style="position:relative;">
 					<input type="text" id="fieldSearch" class="form-control" autocomplete="off" placeholder="Buscar" autocomplete="off" name="q" value="<?php echo get_search_query(); ?>">
 					<input type="hidden" name="post_type" value="post">
 					
-					<div id="formText">
+					<div id="formText" class="d-none">
 						<a href="https://pesquisa.bvsalud.org">Pesquisa via formulário iAH</a> |
 						<a href="https://pesquisa.bvsalud.org">Como pesquisar</a>
 					</div>
