9294d1c683fdd978b76aba85956a46566285184f
Márcio Alves <marcioalvessilvajc@gmail.com>
2022-11-16 09:39:55 -0300

    Ajuste busca interna DeCS/MeSH

--- decs/includes/navInter.php
+++ decs/includes/navInter.php
@@ -1,9 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark" id="nav">
     <div class="container">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
         </button>
-        <button id="btSearchM" class="btn btn-outline-light my-2 my-sm-0 d-block d-lg-none" type="submit" ><i class="fas fa-search"></i></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <?php
                 wp_nav_menu( array(
@@ -17,17 +16,11 @@
                     'walker'            => new WP_Bootstrap_Navwalker())
                 );
             ?>
-            <button id="btSearch" class="btn btn-outline-light my-2 my-sm-0 d-none d-lg-block" type="submit" ><i class="fas fa-search"></i></button>
         </div>
     </div>
 </nav>
-
-<section class="container" id="searchInside" >
-    <div class="row padding2">
-        <div class="col-12">
-
-            <?php get_template_part('includes/search') ?>
-
-        </div>
+<section class="padding3" id="searchInside" >
+    <div class="container">
+        <?php get_template_part('includes/search') ?>
     </div>
 </section>
