f8db7e4d33b979371d3f0bc2ee81b1cc96c49207
Márcio Alves <marcioalvessilvajc@gmail.com>
2023-03-16 09:20:27 -0300

    Ajuste tema Evid@Easy

--- evideasy/template-evideasy.php
+++ evideasy/template-evideasy.php
@@ -3,42 +3,43 @@
 Template Name: Evid@Easy Template
 */
 
-$site_lang = substr($current_language, 0,2);
+$lang = get_locale();
+$site_lang = substr($lang, 0,2);
 
 //include scripts for template page
 function load_scripts_evideasy_template() {
-    if ( is_page_template( 'template-evideasy.php' ) ) {
-        wp_enqueue_style('evideasy-gfonts', 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap', false);
-        wp_enqueue_style('bootstrap-evideasy-style', get_stylesheet_directory_uri() .'/assets-evideasy/css/bootstrap.min.css');
-        wp_enqueue_style('evideasy-style', get_stylesheet_directory_uri() .'/assets-evideasy/css/style.css', array('bootstrap-evideasy-style', 'evideasy-gfonts'));
-        wp_enqueue_script('evideasy-script', get_stylesheet_directory_uri() .'/assets-evideasy/js/scripts.js', array('jquery'), '1.0', false);
-        wp_enqueue_script('evideasy-responsivity-script', get_stylesheet_directory_uri() .'/assets-evideasy/js/responsivity.js', array('jquery'), '1.0', false);
-    }
+  if ( is_page_template( 'template-evideasy.php' ) ) {
+    wp_enqueue_style('evideasy-gfonts', 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap', false);
+    wp_enqueue_style('bootstrap-evideasy-style', get_stylesheet_directory_uri() .'/assets-evideasy/css/bootstrap.min.css');
+    wp_enqueue_style('evideasy-style', get_stylesheet_directory_uri() .'/assets-evideasy/css/style.css', array('bootstrap-evideasy-style', 'evideasy-gfonts'));
+    wp_enqueue_script('evideasy-script', get_stylesheet_directory_uri() .'/assets-evideasy/js/scripts.js', array('jquery'), '1.0', false);
+        //wp_enqueue_script('evideasy-responsivity-script', get_stylesheet_directory_uri() .'/assets-evideasy/js/responsivity.js', array('jquery'), '1.0', false);
+  }
 }
 add_action( 'wp_enqueue_scripts', 'load_scripts_evideasy_template' );
 
 get_header();
 
 if($site_lang == 'pt'){
-    $subtitle = 'Evidências para apoiar suas decisões de saúde';    
-    $label_selections = 'Suas seleções';
+  $subtitle = 'Evidências para apoiar suas decisões de saúde';    
+  $label_selections = 'Suas seleções';
 }
 elseif($site_lang == 'en'){
-    $subtitle = 'Evidence to support your health decisions';
-    $label_selections = 'Your selections';
+  $subtitle = 'Evidence to support your health decisions';
+  $label_selections = 'Your selections';
 }
 elseif($site_lang == 'fr'){
-    $subtitle = 'Des preuves pour étayer vos décisions en matière de santé';
-    $label_selections = 'Vos sélections';
+  $subtitle = 'Des preuves pour étayer vos décisions en matière de santé';
+  $label_selections = 'Vos sélections';
 }
 else{ //es
-    $subtitle = 'Evidencia para respaldar sus decisiones en salud';
-    $label_selections = 'Sus selecciones';
+  $subtitle = 'Evidencia para respaldar sus decisiones en salud';
+  $label_selections = 'Sus selecciones';
 }
 ?>
-<section class="container">
-<div class="middle">
 
+<section class="container">
+  <div class="middle">
     <div class="row">
       <div class="col-md-12">
         <nav aria-label="breadcrumb">
@@ -61,46 +62,46 @@ else{ //es
 
     <div class="row grid-content-search">
       <div class="col-md-9">
-          <h1 id="main-question"></h1>
+        <h1 id="main-question"></h1>
 
-          <div class="card grid-options">
-            <div class="card-body">
-              <ul></ul>
-            </div>
+        <div class="card grid-options">
+          <div class="card-body">
+            <ul></ul>
           </div>
+        </div>
 
       </div>
       <div class="col-md-3">
 
         <div class="grid-selected-options">
-            <label style="display: none;"><?php echo $label_selections; ?></label>
-            <ul></ul>
+          <label style="display: none;"><?php echo $label_selections; ?></label>
+          <ul></ul>
         </div>
         <div class="grid-buttons" style="display: none;">
           <button id="btn-reset" type="button" class="btn btn-outline-primary">Cancelar</button>
           <button id="btn-search" type="button" class="btn btn-primary">Buscar</button>
         </div>
-          
+
       </div>      
     </div>
 
     <?php if(!empty(get_the_content())){ ?>
-    <div class="row">
-      <div class="col-md-12">
-        <?php the_content(); ?>
+      <div class="row">
+        <div class="col-md-12">
+          <?php the_content(); ?>
+        </div>
       </div>
-    </div>
     <?php } ?>
 
     <br>
 
-</div>
+  </div>
 
-<div id="loader-spinner">
+  <div id="loader-spinner">
     <div class="spinner-border"></div>
-</div>
-<div id="loader-spinner-search" style="display: none;">
+  </div>
+  <div id="loader-spinner-search" style="display: none;">
     <div class="spinner-border"></div>
-</div>
+  </div>
 </section>
 <?php get_footer(); ?>
