4000154c7413dadff0b661a1016eafddeef2de57
Márcio Alves <marcioalvessilvajc@gmail.com>
2023-04-14 15:47:35 -0300

    Footer BVS Moçambique

--- bvs-mocambique/footer.php
+++ bvs-mocambique/footer.php
@@ -1,15 +1,33 @@
 <footer id="footer">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-2">
-				<img src="<?php bloginfo('template_directory') ?>/img/logo-ins.jpg" id="logo-ins" alt="INS" class="img-fluid">
+			<div class="col-md-3">
+				<div class="row">
+					<div class="col-6">
+				<a href="https://www.misau.gov.mz" target="_blank">
+					<img src="<?php bloginfo('template_directory') ?>/img/min-saude.png" alt="OMS Nubu" id="" class="img-fluid">
+				</a>
+						
+					</div>
+					<div class="col-6">
+				<a href="https://ins.gov.mz/" target="_blank">
+					<img src="<?php bloginfo('template_directory') ?>/img/ins-nac-saude.png" id="logo-ins" alt="INS" class="img-fluid">
+				</a>
+						
+					</div>
+					<div class="col-12">
+				<a href="https://www.afro.who.int/pt/countries/mozambique" target="_blank">
+					<img src="<?php bloginfo('template_directory') ?>/img/oms-mocambique.png" alt="OMS Moçambique" id="oms-mocambique" class="img-fluid">
+				</a>
+						
+					</div>
+				</div>
 			</div>
-			<div class="col-md-6">
+			<div class="col-md-5">
 				<ul class="list-unstyled"><?php dynamic_sidebar('footer_widget') ?></ul>
 			</div>
 			<div class="col-md-4 text-center">
 				<b>Parceiros</b> <br>
-				<a href="https://www.afro.who.int/pt/countries/mozambique" target="_blank"><img src="<?php bloginfo('template_directory') ?>/img/oms-mocambique.png" alt="OMS Moçambique" id="oms-mocambique" class="img-fluid"></a>
 				<a href="https://www.paho.org/pt/bireme" target="_blank"><img src="http://logos.bireme.org/img/pt/v_bir_white.svg" alt="BIREME" id="logo-bireme" class="img-fluid"></a>
 			</div>
 		</div>
