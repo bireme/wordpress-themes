e8aeff547b387b072cd84c9236e2087f24196e30
Márcio Alves <marcioalvessilvajc@gmail.com>
2023-08-07 10:56:33 -0300

    Ajsute tema Cumbre

--- mtci-eventos/page.php
+++ mtci-eventos/page.php
@@ -1,8 +1,8 @@
 <?php get_header('opas'); ?>
 <?php get_template_part('includes/nav-opas') ?>
 <main id="main_container">
-	<h1 class="title1"><?php the_title(); ?></h1>
 	<div class="container">
+		<h1 class="title1"><?php the_title(); ?></h1>
 		<?php the_content(); ?>
 	</div>
 </main>
