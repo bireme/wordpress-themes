eaaae0a0c2e56c6e8333b05d36aa6a556e1e0bfb
Márcio Alves <marcioalvessilvajc@gmail.com>
2023-08-07 10:48:19 -0300

    Ajuste tema Cumbre

---
+++ mtci-eventos/page.php
@@ -0,0 +1,9 @@
+<?php get_header('opas'); ?>
+<?php get_template_part('includes/nav-opas') ?>
+<main id="main_container">
+	<h1 class="title1"><?php the_title ?></h1>
+	<div class="container">
+		<?php the_content(); ?>
+	</div>
+</main>
+<?php get_footer('opas'); ?>
