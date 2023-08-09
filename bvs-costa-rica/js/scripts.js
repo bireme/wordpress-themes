532005f07d5317901570bb51dbcf85922b02225d
wilsonmoura <wilsonsmoura@gmail.com>
2023-08-09 17:06:48 -0300

    #177 -- Tema BVS Costa Rica: Criar campo de busca com múltiplos filtros

--- bvs-costa-rica/js/scripts.js
+++ bvs-costa-rica/js/scripts.js
@@ -2,9 +2,9 @@ jQuery(function($){
     $(".form-check-input").click(function () {
         var val = $(this).val();
         if ( 'op1' == val ) {
-            $('#input-filter').attr('disabled', false);
-        } else {
             $('#input-filter').attr('disabled', true);
+        } else {
+            $('#input-filter').attr('disabled', false);
         }
     });
 });
