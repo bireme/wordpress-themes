eae773c54c59027ca87dd5b5f42a13d1f3c89e12
wilsonmoura <wilsonsmoura@gmail.com>
2023-09-30 00:52:20 -0300

    Fixed #183 -- Tema BVS APS: Ajustar a seção das SOFs mais recentes

--- wp-bvs-aps/template-parts/item-sof-home.php
+++ wp-bvs-aps/template-parts/item-sof-home.php
@@ -1,10 +1,18 @@
+<?php
+    global $sof;
+    $response = file_get_contents('https://aps-repo.bvs.br/wp-json/wp/v2/teleconsultor?post='.$sof->id);
+    $json = json_decode($response, true);
+    $terms = wp_list_pluck( $json, 'name' ); 
+?>
 <div class="col-md-3 item-sof-home">
-	<div class="content-sof">
-		<label class="date-sof"><?php echo get_the_date('d M Y', get_the_ID()); ?></label>
-		<label class="nucleo-sof"><?php echo get_the_term_list(get_the_ID(), 'teleconsultor', '', ', '); ?></label>
-		<h3 class="title-sof"><a href="<?php echo the_permalink(get_the_ID()); ?>"><?php the_title(); ?></a></h3>
-		<div class="excerpt-sof">
-			<?php crop_text(get_the_excerpt(get_the_ID()), 130); ?>
-		</div>
-	</div>
+    <div class="content-sof">
+        <label class="date-sof"><?php echo date_i18n('d M Y', strtotime($sof->date)); ?></label>
+        <?php if ( $terms ) : ?>
+            <label class="nucleo-sof"><?php echo implode(', ', $terms); ?></label>
+        <?php endif; ?>
+        <h3 class="title-sof"><a href="<?php echo $sof->link; ?>"><?php echo $sof->title->rendered; ?></a></h3>
+        <div class="excerpt-sof">
+            <?php crop_text(wp_strip_all_tags($sof->content->rendered), 130); ?>
+        </div>
+    </div>
 </div>
--- wp-bvs-aps/template-parts/last-sofs.php
+++ wp-bvs-aps/template-parts/last-sofs.php
@@ -1,43 +1,38 @@
 <section id="ultimas-sofs" class="col-md-12">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<?php if ( is_active_sidebar( 'last-sof' ) ) : ?>
-                			<?php dynamic_sidebar( 'last-sof' ); ?>
-            			<?php endif; ?>
-			</div>
-		</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <?php if ( is_active_sidebar( 'last-sof' ) ) : ?>
+                    <?php dynamic_sidebar( 'last-sof' ); ?>
+                <?php endif; ?>
+            </div>
+        </div>
 
-		<div class="row">
-			<?php
-			$args_sof = array(
-				'posts_per_page' => 4,
-				'post_type'  => 'aps',
-				'orderby' => 'date',
-				'order'   => 'DESC',
-			);
-			$sofs = new WP_Query( $args_sof );
-			?>
+        <div class="row">
+            <?php
+                global $sof;
+                $response = file_get_contents('https://aps-repo.bvs.br/wp-json/wp/v2/aps?per_page=4');
+                $sofs = json_decode($response);
+            ?>
 
-			<?php if ( $sofs->have_posts() ) : 
-				while ( $sofs->have_posts() ) : $sofs->the_post(); 
+            <?php if ( $sofs ) :
+                foreach ($sofs as $sof) :
 
-					get_template_part('template-parts/item-sof', 'home');
+                    get_template_part('template-parts/item-sof', 'home');
 
-				endwhile; ?>
-				<?php wp_reset_postdata(); ?>
-			<?php endif; ?>
-		</div>
+                endforeach; ?>
+            <?php endif; ?>
+        </div>
 
-		<div class="row">
-			<div class="col-md-8">
-				<p class="feed-rss"><?php _e('Quero receber as <b>últimas SOF</b> por', 'bvs_lang'); ?> <a target="_blank" href="<?php bloginfo('rss2_url'); ?>?post_type=aps"><u>RSS</u> <span class="fas fa-rss-square"></span></a></p>
-			</div>
-			<div class="col-md-4 grid-btn">
-				<a href="<?php echo get_post_type_archive_link('aps'); ?>" class="btn btn-primary btn-sm">
-					<?php _e('Veja mais', 'bvs_lang'); ?> <span class="fas fa-arrow-right"></span>
-				</a>
-			</div>
-		</div>
-	</div>
+        <div class="row">
+            <div class="col-md-8">
+                <p class="feed-rss"><?php _e('Quero receber as <b>últimas SOF</b> por', 'bvs_lang'); ?> <a target="_blank" href="https://aps-repo.bvs.br/feed/?post_type=aps"><u>RSS</u> <span class="fas fa-rss-square"></span></a></p>
+            </div>
+            <div class="col-md-4 grid-btn">
+                <a href="https://aps-repo.bvs.br/aps/" class="btn btn-primary btn-sm">
+                    <?php _e('Veja mais', 'bvs_lang'); ?> <span class="fas fa-arrow-right"></span>
+                </a>
+            </div>
+        </div>
+    </div>
 </section>
