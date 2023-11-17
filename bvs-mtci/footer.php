61a6bf3e908705a07f68dd441624be01126bcf7e
Márcio Alves <marcioalvessilvajc@gmail.com>
2023-11-17 14:17:47 -0300

    Ajuste footer BVS MTCI

--- bvs-mtci/footer.php
+++ bvs-mtci/footer.php
@@ -12,7 +12,7 @@
 	</div>
 </footer>
 <div id="footer-term" class="text-center">
-	<a href="https://politicas.bireme.org/terminos/<?php echo $lang=='fr'?'en':$lang; ?>/" target="_blank"><?php _e('Terms and Conditions of Use', 'mtci'); ?></a> | 
+	La responsabilidad de los contenidos de la BVS MTCI es de la Red MTCI Américas | <a href="https://politicas.bireme.org/terminos/<?php echo $lang=='fr'?'en':$lang; ?>/" target="_blank"><?php _e('Terms and Conditions of Use', 'mtci'); ?></a> | 
 	<a href="https://politicas.bireme.org/privacidad/<?php echo $lang=='fr'?'en':$lang; ?>/" target="_blank"><?php _e('Privacy policy', 'mtci'); ?><?php pll_e('Privacy policy'); ?></a>
 </div>
 </body>
