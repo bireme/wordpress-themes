9c6b1451e724e4443fac0ed36407665a48c6e874
Márcio Alves <marcioalvessilvajc@gmail.com>
2023-12-13 18:46:00 -0300

    Tema WordPressBVS Uruguai

---
+++ bvs-uruguay/footer.php
@@ -0,0 +1,23 @@
+		<footer id="footer">
+			<div class="container">
+				<ul type="none"><?php dynamic_sidebar('footer') ?></ul>
+				<hr>	
+				<p class="text-center"><a href="">Termos e Condições de uso</a> | <a href="">Políticas de Privacidade</a></p>
+			</div>
+		</footer>
+		<div id="powered">
+			<img src="<?php bloginfo('template_directory') ;?>/img/powered.png" alt="">
+		</div>
+		<div id="to-top" class="to-top">
+			<span class="float-left">
+				<i class="bi bi-arrow-up-circle-fill"></i>
+			</span>
+		</div>
+		<div id="social" class="d-print-none">
+			<ul>
+				<ul type="none"><?php dynamic_sidebar('social') ?></ul>
+			</ul>
+		</div>
+		<?php wp_footer(); ?>
+	</body>
+</html>
---
+++ bvs-uruguay/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<?php wp_body_open(); ?>
+	<header id="header">
+		<div class="container">
+			<div class="row" style="position:relative;">
+				<div class="col-lg-6" id="logo-bvs">
+					<a href="<?php echo get_option('siteurl'); ?>/<?php echo $lang=='es'?'':$lang; ?>">
+						<img src="http://logos.bireme.org/img/es/bvs_color.svg" alt="BVS" id="header-bvs" class="float-start">
+					</a>
+
+					<h1>
+						<small><?php echo get_option('blogdescription'); ?></small><br>
+						<img src="<?php bloginfo('template_directory') ?>/img/flag.svg" alt=""  id="flag"><?php echo get_option('blogname'); ?>
+					</h1>
+				</div>
+
+			</div>
+		</div>
+	</header>
+	<?php wp_head(); ?>
+
