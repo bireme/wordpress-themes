ba4ac3efc892680a4dce71fc2f68f6916fea0e44
Márcio Alves <marcioalvessilvajc@gmail.com>
2023-12-18 22:55:08 -0300

    Ajustes no Tema Portal Regional

--- portal-regional/footer.php
+++ portal-regional/footer.php
@@ -1,23 +1,26 @@
+		<?php $lang = pll_current_language(); ?>
 		<footer id="footer">
 			<div class="container">
 				<div class="row">
-					<div class="col-lg-6">
+					<div class="col-lg-8">
+						<img src="<?php bloginfo('template_directory') ;?>/img/bvs-footer-<?php echo $lang; ?>.svg" alt="BVS" id="footer-bvs" class="float-start">
 						<ul type="none"><?php dynamic_sidebar('footer1') ?></ul>
 					</div>
-					<div class="col-lg-6" id="footer-bireme">
+					<div class="col-lg-4" id="footer-bireme">
 						<ul type="none"><?php dynamic_sidebar('footer2') ?></ul>
 					</div>
 				</div>
-				<hr>	
-				<p class="text-center">
-					<a href="https://politicas.bireme.org/terminos/<?php echo $idioma=='fr'?'en':$idioma; ?>/" target="_blank"><?php pll_e('Terms and conditions of use'); ?></a> | 
-					<a href="https://politicas.bireme.org/privacidad/<?php echo $idioma=='fr'?'en':$idioma; ?>/" target="_blank"><?php pll_e('Privacy policy'); ?></a>
-				</p>
+				<hr>
+				<div class="row">
+					<div class="col-md-6" id="copyright">
+						© BIREME / OPAS / OMS. Todo os direitos são reservados.
+					</div>
+					<div class="col-md-6" id="powered">
+						<img src="<?php bloginfo('template_directory') ;?>/img/powered.png" alt="">
+					</div>
+				</div>	
 			</div>
 		</footer>
-		<div id="powered">
-			<img src="<?php bloginfo('template_directory') ;?>/img/powered.png" alt="">
-		</div>
 		<div id="to-top" class="to-top">
 			<span class="float-left">
 				<i class="bi bi-arrow-up-circle-fill"></i>
@@ -30,4 +33,4 @@
 		</div>
 		<?php wp_footer(); ?>
 	</body>
-</html>
+	</html>
