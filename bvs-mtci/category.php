9f1e88d5230ea23bd982ccd808bfa6b445e35d87
Márcio Alves <marcioalvessilvajc@gmail.com>
2024-05-22 12:00:08 -0300

    Ajustes breadcrumb tema BVS MTCI

--- bvs-mtci/category.php
+++ bvs-mtci/category.php
@@ -1,10 +1,12 @@
 <?php get_header(); ?>
+<?php $lang = pll_current_language(); ?>
 <?php get_template_part('includes/nav') ?>
 <main id="main_container">
 	<section id="title">
 		<div class="container">
 			<h1 class="title1"><?php $cat = get_the_category(); echo $cat[0]->cat_name; ?></h1>
 			<div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
+				<a href="<?php echo get_option('siteurl'); ?>/<?php echo $lang=='es'?'':$lang; ?>">Home</a>
 				<?php if(function_exists('bcn_display'))
 				{
 					bcn_display();
