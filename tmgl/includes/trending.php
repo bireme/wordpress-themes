eb30ad47b167528b71cccbd1c83b4ef860e151af
Márcio Alves <marcioalvessilvajc@gmail.com>
2024-09-05 09:18:22 -0300

    Atualizar tema wordpress TMGL

--- tmgl/includes/trending.php
+++ tmgl/includes/trending.php
@@ -22,32 +22,32 @@ if (function_exists('have_rows')) {
 	</div>
 	<div class="col-md-9">
 		<div class="trending-slider">
-		<?php
-		$args = array(
-			'post_type' => 'trending_topics',
-			'posts_per_page' => 6,
-		);
-		$trending_topics_query = new WP_Query($args);
-		if ($trending_topics_query->have_posts()) : 
-			while ($trending_topics_query->have_posts()) : $trending_topics_query->the_post(); ?>
-				<article class="col" style="margin: 0 15px; padding-bottom: 20px;">
-					<div class="card card-trend ">
-						<div class="card-body">
-							<h5 class="card-title"><?php the_title(); ?></h5>
-							<p class="card-text"><?php the_excerpt(); ?></p>
+			<?php
+			$args = array(
+				'post_type' => 'trending_topics',
+				'posts_per_page' => 6,
+			);
+			$trending_topics_query = new WP_Query($args);
+			if ($trending_topics_query->have_posts()) : 
+				while ($trending_topics_query->have_posts()) : $trending_topics_query->the_post(); ?>
+					<article class="col" style="margin: 0 15px; padding-bottom: 20px;">
+						<div class="card card-trend ">
+							<div class="card-body">
+								<h5 class="card-title"><?php the_title(); ?></h5>
+								<p class="card-text"><?php the_excerpt(); ?></p>
+							</div>
+							<div class="card-footer text-end">
+								<small class="text-body-secondary"><a href="<?php the_permalink(); ?>" class="btn btn-primary btn-sm"><i class="bi bi-arrow-right"></i></a></small>
+							</div>
 						</div>
-						<div class="card-footer text-end">
-							<small class="text-body-secondary"><a href="<?php the_permalink(); ?>" class="btn btn-primary btn-sm"><i class="bi bi-arrow-right"></i></a></small>
-						</div>
-					</div>
-				</article>
-			<?php endwhile;
-			wp_reset_postdata();
-		else : 
-			echo '<p>Trending Topic not found.</p>';
-		endif;
-		?>
-	</div>
+					</article>
+				<?php endwhile;
+				wp_reset_postdata();
+			else : 
+				echo '<p>Trending Topic not found.</p>';
+			endif;
+			?>
+		</div>
 	</div>
 	<p class="mt-4"><?php _e( 'Explore all trending topics', 'tmgl' ); ?> <a href="<?= $url_trending_topics;?>" class="btn btn-primary btn-sm"><i class="bi bi-arrow-right"></i></a></p>
 </div>
