6212de4053503859aa9920d63dd48f411fe88335
Márcio Alves <48724136+marcioalvessilva@users.noreply.github.com>
2024-09-20 10:06:57 -0300

    Ajuste tema WordPress TMGL

--- tmgl/functions.php
+++ tmgl/functions.php
@@ -212,6 +212,61 @@ function create_dimensions_cpt() {
     register_post_type('dimensions', $args);
 }
 add_action('init', 'create_dimensions_cpt', 0);
+function create_navegation_cpt() {
+    $labels = array(
+        'name'                      => _x('Navegations', 'Post Type General Name', 'textdomain'),
+        'singular_name'             => _x('Navegation', 'Post Type Singular Name', 'textdomain'),
+        'menu_name'                 => __('Navegations', 'textdomain'),
+        'name_admin_bar'            => __('Navegation', 'textdomain'),
+        'archives'                  => __('Item Archives', 'textdomain'),
+        'attributes'                => __('Item Attributes', 'textdomain'),
+        'parent_item_colon'         => __('Parent Item:', 'textdomain'),
+        'all_items'                 => __('All Items', 'textdomain'),
+        'add_new_item'              => __('Add New Item', 'textdomain'),
+        'add_new'                   => __('Add New', 'textdomain'),
+        'new_item'                  => __('New Item', 'textdomain'),
+        'edit_item'                 => __('Edit Item', 'textdomain'),
+        'update_item'               => __('Update Item', 'textdomain'),
+        'view_item'                 => __('View Item', 'textdomain'),
+        'view_items'                => __('View Items', 'textdomain'),
+        'search_items'              => __('Search Item', 'textdomain'),
+        'not_found'                 => __('Not found', 'textdomain'),
+        'not_found_in_trash'        => __('Not found in Trash', 'textdomain'),
+        'featured_image'            => __('Featured Image', 'textdomain'),
+        'set_featured_image'        => __('Set featured image', 'textdomain'),
+        'remove_featured_image'     => __('Remove featured image', 'textdomain'),
+        'use_featured_image'        => __('Use as featured image', 'textdomain'),
+        'insert_into_item'          => __('Insert into item', 'textdomain'),
+        'uploaded_to_this_item'     => __('Uploaded to this item', 'textdomain'),
+        'items_list'                => __('Items list', 'textdomain'),
+        'items_list_navigation'     => __('Items list navigation', 'textdomain'),
+        'filter_items_list'         => __('Filter items list', 'textdomain'),
+    );
+    $args = array(
+        'label'                     => __('Navegation', 'textdomain'),
+        'description'               => __('Post Type for Navegations', 'textdomain'),
+        'labels'                    => $labels,
+        'supports'                  => array('title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'custom-fields', 'page-attributes', 'post-formats'),
+        'hierarchical'              => true,
+        'public'                    => true,
+        'show_ui'                   => true,
+        'show_in_menu'              => true,
+        'menu_position'             => 5,
+        'show_in_admin_bar'         => true,
+        'show_in_nav_menus'         => true,
+        'can_export'                => true,
+        'has_archive'               => true,
+        'exclude_from_search'       => false,
+        'publicly_queryable'        => true,
+        'capability_type'           => 'post',
+        'show_in_rest'              => true, // Suport Editor Gutenberg
+        'menu_icon'                 => 'dashicons-admin-site-alt3',
+
+    );
+    register_post_type('navegation', $args);
+}
+add_action('init', 'create_navegation_cpt', 0);
+
 flush_rewrite_rules();
 function custom_excerpt_length($length) {
     return 20; // Substitua 100 pelo número de palavras que você deseja
