2812c635be48fa9d51e70ec45935d86fd965fe22
Marcio <48724136+marcioalvessilva@users.noreply.github.com>
2024-11-25 16:04:15 -0300

    Ajuste pagina 404 tema WordPress LILACS

--- lilacs2019/404.php
+++ lilacs2019/404.php
@@ -6,10 +6,10 @@ get_header();?>
 		<div class="row 404page">
 			<div class="col-md-12 text-center">
 				<p class="text-center"><h1 style="text-align: center;">Erro 404</h1></p>
-				<p class="text-center"><img src="<?php echo get_bloginfo( 'stylesheet_directory' );?>/images/404.jpg')" alt="404"/></p>
+				<p class="text-center"><img src="<?php echo get_bloginfo( 'stylesheet_directory' );?>/images/404.jpg" alt="404"/></p>
 			</div>
 			<div class="col-md-12">
-				<p class="text-center">OOOOOOOOOOOOOOOOOOps! Lamentamos, mas a página procurada não existe ou foi apagada.</p>
+				<p class="text-center">Ops! Lamentamos, mas a página procurada não existe ou foi apagada.</p>
 				<p class="text-center">Você pode navegar através do menu abaixo:</p>
 				<p class="text-center">
 					<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
