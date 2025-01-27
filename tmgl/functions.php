e4ee2f6c9db7f4ae3bc5c4a730e0f94c11e39838
= <=>
2025-01-27 15:34:57 -0300

    add ctp resources

--- tmgl/functions.php
+++ tmgl/functions.php
@@ -33,6 +33,7 @@ function tmgl_register_menus()
         'regional-menu' => __('Regional Menu', 'tmgl'),
         'footer-left' => __('Footer Left', 'tmgl'),
         'footer-right' => __('Footer Right', 'tmgl'),
+        'footer-right-top' => __('Footer Right Top', 'tmgl'),
         'footer-center' => __('Footer Center', 'tmgl')
     ));
 }
@@ -52,6 +53,65 @@ register_sidebar([
     'before_title' => '<h5>',
     'after_title' => '</h5>'
 ]);
+
+function create_resources_cpt()
+{
+    $labels = array(
+        'name' => _x('Resources', 'Additional Resources for objects from external sources', 'textdomain'),
+        'singular_name' => _x('Resource', 'Post Type Singular Name', 'textdomain'),
+        'menu_name' => __('Resources', 'textdomain'),
+        'name_admin_bar' => __('Resources', 'textdomain'),
+        'archives' => __('Item Resources', 'textdomain'),
+        'attributes' => __('Item Resources', 'textdomain'),
+        'parent_item_colon' => __('Parent Item:', 'textdomain'),
+        'all_items' => __('All Items', 'textdomain'),
+        'add_new_item' => __('Add New Item', 'textdomain'),
+        'add_new' => __('Add New', 'textdomain'),
+        'new_item' => __('New Item', 'textdomain'),
+        'edit_item' => __('Edit Item', 'textdomain'),
+        'update_item' => __('Update Item', 'textdomain'),
+        'view_item' => __('View Item', 'textdomain'),
+        'view_items' => __('View Items', 'textdomain'),
+        'search_items' => __('Search Item', 'textdomain'),
+        'not_found' => __('Not found', 'textdomain'),
+        'not_found_in_trash' => __('Not found in Trash', 'textdomain'),
+        'featured_image' => __('Featured Image', 'textdomain'),
+        'set_featured_image' => __('Set featured image', 'textdomain'),
+        'remove_featured_image' => __('Remove featured image', 'textdomain'),
+        'use_featured_image' => __('Use as featured image', 'textdomain'),
+        'insert_into_item' => __('Insert into item', 'textdomain'),
+        'uploaded_to_this_item' => __('Uploaded to this item', 'textdomain'),
+        'items_list' => __('Items list', 'textdomain'),
+        'items_list_navigation' => __('Items list navigation', 'textdomain'),
+        'filter_items_list' => __('Filter items list', 'textdomain'),
+    );
+    $args = array(
+        'label' => __('Resources', 'textdomain'),
+        'description' => __('Post Type for Resources', 'textdomain'),
+        'labels' => $labels,
+        'supports' => array('title', 'thumbnail', 'custom-fields', 'post-formats'),
+        'taxonomies' => array('category', 'post_tag'), // Adicionando categorias e tags padrão
+        'hierarchical' => true,
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'menu_position' => 5,
+        'show_in_admin_bar' => true,
+        'show_in_nav_menus' => true,
+        'can_export' => true,
+        'has_archive' => true,
+        'exclude_from_search' => false,
+        'publicly_queryable' => true,
+        'capability_type' => 'post',
+        'show_in_rest' => true, // Suport Editor Gutenberg
+        'menu_icon' => 'dashicons-rest-api',
+
+    );
+    register_post_type('videos', $args);
+}
+add_action('resources', 'create_resources_cpt', 0);
+
+
 
 // Custom Post Type Trending Topics
 function create_trending_topics_cpt()
