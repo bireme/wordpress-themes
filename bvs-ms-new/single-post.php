6d70a23b3d70d672742323ebf3a507c1055eb503
Da Silva <dasilvmar@paho.org>
2025-05-23 16:34:43 -0300

    Ajuste tema wordpress bvs ms

--- bvs-ms-new/single-post.php
+++ bvs-ms-new/single-post.php
@@ -1,20 +1,39 @@
 <?php get_header(); ?>
 <?php get_template_part('includes/nav') ?>
-<section class="margin4">
-	<div class="container">
+<main id="main_container">
+	<div class="container pt-4 pb-5">
+		<div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
+			<?php if(function_exists('bcn_display'))
+			{
+				bcn_display();
+			}?>
+		</div>
+		<h1 class="title1"><?php the_title(); ?></h1>
+		<div class="mt-5">
+			<?php if (has_post_thumbnail()) : ?>
+				<div class="single-thumb">
+					<?php the_post_thumbnail('full', array('class' => 'img-fluid')); ?>
+				</div>
+			<?php endif; ?>
+			<?php the_content(); ?>
+			<br>
+			<hr class="border">	
+			<?php
+			setlocale(LC_TIME, 'pt_BR.UTF-8');
+			echo 'Publicado: '. strftime('%A, %d de %B de %Y', strtotime(get_the_date()));
+			?>
+
+			<div class="clearfix mt-5"></div>
+		</div>
 		<div class="row">
-			<div class="col-md-12">
-				<h1 class="title1"><?php the_title(); ?></h1> <br>
-				<?php while(have_posts()) : the_post();	?>
-					<?php the_post_thumbnail('medium',['class' => 'img-fluid imgPost']); ?>
-					<?php the_content();
-				endwhile;
-				?>
+			<hr class="border">	
+			<div class="col-12 col-md-4">
+				<?php previous_post_link( '%link' ); ?>  
+			</div>
+			<div class="col-12 offset-md-4 col-md-4 text-end">
+				<?php next_post_link( '%link'); ?> 
 			</div>
 		</div>
 	</div>
-</section>
-<div class="color1">
-	<?php get_template_part('includes/noticias') ?>
-</div>
+</main>
 <?php get_footer(); ?>
