7343b269dba7adfe18fd5c26a9482f4adf139cc8
Da Silva <dasilvmar@paho.org>
2025-06-17 10:25:28 -0300

    Ajuste tema WP DeCS Finder

--- DeCSFinder/header.php
+++ DeCSFinder/header.php
@@ -14,32 +14,34 @@
 		<div class="container">
 			<div class="row" style="position:relative;">
 				<div class="col-md-4" id="brand">
-					<a href="<?php echo get_option('siteurl'); ?>/<?php echo $lang=='pt'?'':$lang; ?>"><img src="<?php bloginfo('template_directory'); ?>/img/logo-<?php echo $lang; ?>.svg" alt=""></a>
+					<a href="<?php echo get_option('siteurl'); ?>/<?php echo $lang=='pt'?'':$lang; ?>">
+						<img src="<?php bloginfo('template_directory'); ?>/img/logo-<?php echo $lang; ?>.svg" id="" alt="Logo DeCS Finder">
+					</a>
 				</div>
 				<div class="col-md-8">
 					<?php 
-						wp_nav_menu( array(
-							'theme_location'    => 'main-nav',
-							'depth'             => 1,
-							'container'         => 'ul',
-							'container_class'   => 'list-unstyled',
-							'container_id'      => '',
-							'menu_class'        => '',
-						) );
-						?>
-					</div>
-					<div id="lang">
-						<?php 
-						wp_nav_menu( array(
-							'theme_location'    => 'Language',
-							'depth'             => 1,
-							'container'         => 'ul',
-							'container_class'   => 'list-unstyled',
-							'container_id'      => '',
-							'menu_class'        => '',
-						) );
-						?>
-					</div>
+					wp_nav_menu( array(
+						'theme_location'    => 'main-nav',
+						'depth'             => 1,
+						'container'         => 'ul',
+						'container_class'   => 'list-unstyled',
+						'container_id'      => '',
+						'menu_class'        => '',
+					) );
+					?>
+				</div>
+				<div id="lang">
+					<?php 
+					wp_nav_menu( array(
+						'theme_location'    => 'Language',
+						'depth'             => 1,
+						'container'         => 'ul',
+						'container_class'   => 'list-unstyled',
+						'container_id'      => '',
+						'menu_class'        => '',
+					) );
+					?>
 				</div>
 			</div>
-		</header>
+		</div>
+	</header>
