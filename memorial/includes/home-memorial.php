7c659db759a4b2b00aff5e4314efbf6ca0e31ae0
Da Silva <dasilvmar@paho.org>
2025-09-25 10:29:45 -0300

    Ajuste tema memorial da pandemia

--- memorial/includes/home-memorial.php
+++ memorial/includes/home-memorial.php
@@ -6,7 +6,7 @@
 			</div>
 			<div class="col-md-6 d-flex flex-column justify-content-center">
 				<h2 class="title">O Memorial</h2>
-				<p>O Memorial Digital da Pandemia de Covid-19 é uma iniciativa do Centro Cultural do Ministério da Saúde (CCMS) para preservar e dar acesso às memórias da pandemia no Brasil. Desde 11 de março de 2020, quando a Organização Mundial da Saúde decretou estado de pandemia por conta da propagação do Novo Coronavírus, pessoas em todo o mundo começaram a registrar as suas experiências de vida durante esse evento tão assustador e incerto. No Brasil não foi diferente: cidadãs e cidadãos brasileiros vêm registrando continuamente as suas memórias da pandemia. Preservar esses registros é fazer justiça a todas e todos que sofreram durante esse evento, dando continuidade à luta de muitas pessoas para que se construa uma memória e uma história da pandemia no Brasil baseada na ética, no respeito e na cidadania.  <br><br>
+				<p>O Memorial Digital da Pandemia de Covid-19 é uma iniciativa do Centro Cultural do Ministério da Saúde (CCMS) para preservar e dar acesso às memórias da pandemia no Brasil. Desde 11 de março de 2020, quando a Organização Mundial da Saúde decretou estado de pandemia por conta da propagação do Novo Coronavírus, pessoas em todo o mundo começaram a registrar as suas experiências de vida durante esse evento tão assustador e incerto. No Brasil não foi diferente: cidadãs e cidadãos brasileiros vêm registrando continuamente as suas memórias da pandemia.<br><br>
 				<a href="https://memorialpandemia.teste.bvs.br/o-memorial/"><b>Clique aqui e conheça mais sobre o museu </b></a>
 			</div>
 		</div>
--- memorial/includes/home-products.php
+++ memorial/includes/home-products.php
@@ -1,12 +1,47 @@
 <section class="pt-5 pb-5">
 	<div class="container">
-		<h2 class="title text-center">Recursos</h2>
-		<p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing, elit. Nihil saepe quae nobis ab, voluptates at! Molestias omnis minima quo, pariatur. Eligendi, quas? Nihil ut mollitia, dolorem ratione, esse iste velit? <br><br></p>
+		<h2 class="title text-center">Ferramentas</h2>
+		<p class="text-center">Produção Científica e Técnica sobre a Pandemia de COVID-19  organizadas e produzidas pela BIREME entre 2020 e 2025, considerando sua relevância para a gestão do conhecimento, o enfrentamento da infodemia e a preservação documental.<br><br></p>
 		<div class="home-products">
 			<div class="card" style="width: 18rem;">
 				<img src="<?php bloginfo('template_directory'); ?>/img/logo-bvs.png" class="card-img-top" alt="...">
 				<div class="card-body">
 					<h5 class="card-title">BVS</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+					<a href="#" class="btn btn-sm btn-primary"><i class="bi bi-arrow-right-short"></i></a>
+				</div>
+			</div>
+			<div class="card" style="width: 18rem;">
+				<img src="<?php bloginfo('template_directory'); ?>/img/logo-vitrine.png" class="card-img-top" alt="...">
+				<div class="card-body">
+					<h5 class="card-title">Vitrine dos Conhecimento</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk.</p>
+					<a href="#" class="btn btn-sm btn-primary"><i class="bi bi-arrow-right-short"></i></a>
+				</div>
+			</div>
+
+			<div class="card" style="width: 18rem;">
+				<img src="<?php bloginfo('template_directory'); ?>/img/mapas.png" class="card-img-top" alt="...">
+				<div class="card-body">
+					<h5 class="card-title">Mapas de Evidencias</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+					<a href="#" class="btn btn-sm btn-primary"><i class="bi bi-arrow-right-short"></i></a>
+				</div>
+			</div>
+			
+			<div class="card" style="width: 18rem;">
+				<img src="<?php bloginfo('template_directory'); ?>/img/decs.png" class="card-img-top" alt="...">
+				<div class="card-body">
+					<h5 class="card-title">DeCS</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+					<a href="#" class="btn btn-sm btn-primary"><i class="bi bi-arrow-right-short"></i></a>
+				</div>
+			</div>
+
+			<div class="card" style="width: 18rem;">
+				<img src="<?php bloginfo('template_directory'); ?>/img/database.png" class="card-img-top" alt="...">
+				<div class="card-body">
+					<h5 class="card-title">Bases de Dados</h5>
 					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
 					<a href="#" class="btn btn-sm btn-primary"><i class="bi bi-arrow-right-short"></i></a>
 				</div>
@@ -21,14 +56,6 @@
 				</div>
 			</div>
 
-			<div class="card" style="width: 18rem;">
-				<img src="<?php bloginfo('template_directory'); ?>/img/logo-vitrine.png" class="card-img-top" alt="...">
-				<div class="card-body">
-					<h5 class="card-title">Vitrine dos Conhecimento</h5>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk.</p>
-					<a href="#" class="btn btn-sm btn-primary"><i class="bi bi-arrow-right-short"></i></a>
-				</div>
-			</div>
 
 			<div class="card" style="width: 18rem;">
 				<img src="<?php bloginfo('template_directory'); ?>/img/logo-eblueinfo.png" class="card-img-top" alt="...">
