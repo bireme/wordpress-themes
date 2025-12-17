e8370e888aff73110fcbf26f7138ca38e579408e
da Silva <dasilvmar@paho.org>
2025-12-17 15:31:56 -0300

    Ajuste Footer DeCS Finder

--- DeCSFinder/footer.php
+++ DeCSFinder/footer.php
@@ -14,7 +14,7 @@
 	</footer>
 	<div id="powered">
 		<div class="container">
-			<img src="<?php bloginfo('template_directory') ;?>/img/powered-pt.svg" alt="BIREME"><br>
+			<img src="<?php bloginfo('template_directory') ;?>/img/powered-<?php echo $lang; ?>.svg" alt="BIREME"><br>
 			<small>© <?php pll_e('All rights are reserved'); ?></small> <br>
 		</div>
 	</div>
