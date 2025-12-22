0cfcc0c3ce7ae4c752e4f3d670ca2b798011fb12
da Silva <dasilvmar@paho.org>
2025-12-22 08:05:13 -0300

    Ajuste tema Memorial (Footer)

--- memorial/js/main.js
+++ memorial/js/main.js
@@ -41,8 +41,8 @@ jQuery('.home-profiles').slick({
   arrows: false,
   autoplay: true,
   speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 3,
+  slidesToShow: 4,
+  slidesToScroll: 4,
   responsive: [
     {
       breakpoint: 1024,
