a0bf3e5f4f2f7aeb598107a164b0d34339a940f1
da Silva <dasilvmar@paho.org>
2026-01-25 15:02:53 -0300

    Ajuste tema Memorial

---
+++ memorial/includes/page-white.php
@@ -0,0 +1,16 @@
+<?php /* Template Name: Default White */ ?>
+<?php get_header('interno-home'); ?>
+<?php 
+$thumb = get_the_post_thumbnail_url(get_the_ID(), 'full'); 
+$bg = $thumb ? $thumb : get_template_directory_uri() . '/img/header-memorial.jpg';
+?>
+	<main id="main_container" class="mb-5">
+		<div class="container">
+			<div class="breadcrumb">
+				<?php if (function_exists('bcn_display')) { bcn_display(); } ?>
+			</div>
+			<h1><?php the_title(); ?></h1>
+			<?php the_content(); ?>
+		</div>
+	</main>
+<?php get_footer('home'); ?>
