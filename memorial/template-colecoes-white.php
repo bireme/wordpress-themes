98e94bdca721ba30ad4dd36c9a39a1e0f65a2ead
da Silva <dasilvmar@paho.org>
2026-02-02 09:29:55 -0300

    Ajuste tema Memorial (template coleções)

---
+++ memorial/template-colecoes-white.php
@@ -0,0 +1,23 @@
+<?php /*
+ * Template Name: Coleções - White
+ * Template Post Type: colecoes
+*/ ?>
+<?php get_header('interno-home'); ?>
+<?php 
+$thumb = get_the_post_thumbnail_url(get_the_ID(), 'full'); 
+$bg = $thumb ? $thumb : get_template_directory_uri() . '/img/header-memorial.jpg';
+?>
+<div class="title-white">
+	<div class="container">
+		<h1><?php the_title(); ?></h1>
+	</div>
+</div>
+<main id="main_container" class="mb-5">
+	<div class="container">
+		<div class="breadcrumb">
+			<?php if (function_exists('bcn_display')) { bcn_display(); } ?>
+		</div>
+		<?php the_content(); ?>
+	</div>
+</main>
+<?php get_footer('home'); ?>
