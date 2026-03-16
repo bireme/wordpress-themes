6bd79c89c305070f87a85b3b62c8e20ed3727e41
da Silva <dasilvmar@paho.org>
2026-03-16 17:03:38 -0300

    Ajuste tema Memorial (Novidades home)

--- memorial/includes/home-news.php
+++ memorial/includes/home-news.php
@@ -9,7 +9,7 @@ $posts_home = new WP_Query([
 if ( $posts_home->have_posts() ) : ?>
 	<section class="mt-5 mb-5 container">
 		<div class="d-flex align-items-center justify-content-between mb-3">
-			<h2 class="title">Notícias e Destaques</h2>
+			<h2 class="title">Novidades</h2>
 
 			<?php
 			// Ajuste aqui o destino do "Ver todas"
@@ -81,7 +81,7 @@ if ( $posts_home->have_posts() ) : ?>
 
 	</div>
 	<div class="mt-3 text-center me-5 mt-5">
-		<a href="<?php echo esc_url( $blog_url ); ?>/noticias" class="btn btn-primary">
+		<a href="<?php echo esc_url( $blog_url ); ?>/novidades" class="btn btn-primary">
 			Ver todas
 		</a>
 	</div>
