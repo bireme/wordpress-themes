1b3071c1c17d0451337417293215f78b2d15caa5
Antonio <toni@2wp.com.br>
2026-04-02 11:29:10 -0300

    Adicionado o ajuste visual imagem destacada interna do post + ajuste obnject fit para imagens do loop de depoimentos

--- memorial/single-post.php
+++ memorial/single-post.php
@@ -5,7 +5,7 @@
 			<?php if (function_exists('bcn_display')) { bcn_display(); } ?>
 		</div>
 		<h1 class="title"><?php  the_title(); ?></h1>
-		<div class="float-start me-5 mb-4">
+		<div class="float-start imagem-inicial-destacada me-5 mb-4">
 			
 		<?php
 		if ( has_post_thumbnail() ) { 
