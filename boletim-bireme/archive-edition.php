4919a62038852bce6295c771b48b2edfe8e863f7
da Silva <dasilvmar@paho.org>
2026-04-24 14:38:26 -0300

    Ajuste tema Boletim

--- boletim-bireme/archive-edition.php
+++ boletim-bireme/archive-edition.php
@@ -17,23 +17,11 @@
  */
 
 global $wp_query;
+$args = array_merge( $wp_query->query_vars, array( 'orderby' => 'meta_value', 'meta_key' => 'date' ) );
+query_posts($args);
+$wp_query->is_search = false;
 
-$args = $wp_query->query_vars;
-
-$args['nopaging'] = true;
-$args['posts_per_page'] = -1;
-$args['posts_per_archive_page'] = -1;
-
-$args['meta_key'] = 'date';
-$args['orderby'] = 'meta_value';
-$args['order'] = 'DESC';
-
-unset( $args['paged'] );
-unset( $args['page'] );
-
-query_posts( $args );
-
-$wp_query->is_search = false; ?>
+get_header(); ?>
 
 	<main id="content" class="<?php echo odin_classes_page_sidebar(); ?>" tabindex="-1" role="main">
 			<div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
