48cd77839d400b7762b98e89aeaf09def467f0b9
da Silva <dasilvmar@paho.org>
2026-07-28 15:45:42 -0300

    Ajuste tema Memorial (Template Notícias)

--- memorial/noticias.php
+++ memorial/noticias.php
@@ -1,9 +1,14 @@
 <?php /* Template Name: Notícias */ ?>
 <?php get_header('interno'); ?>
+<?php $lang = pll_current_language(); ?>
 <main id="main_container" class="mb-5">
 	<div class="container">
 		<div class="breadcrumb mt-3">
-			<?php if ( function_exists('bcn_display') ) { bcn_display(); } ?>
+			<a href="<?php echo get_option('siteurl'); ?>/<?php echo $lang=='pt'?'':$lang; ?>">HOME</a>
+				<?php if(function_exists('bcn_display'))
+				{
+					bcn_display();
+				}?>
 		</div>
 		<h1 class="title"><?php the_title(); ?></h1>
 		<?php
