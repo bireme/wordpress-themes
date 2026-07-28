a9886451934849030b1baed494a119cd5e3a1c22
da Silva <dasilvmar@paho.org>
2026-07-28 15:21:45 -0300

    Ajuste tema memorial (Breadcrumb News)

--- memorial/category.php
+++ memorial/category.php
@@ -3,7 +3,11 @@
 <main id="main_container" class="mb-5">
 	<div class="container">
 		<div class="breadcrumb mt-3">
-			<?php if ( function_exists('bcn_display') ) { bcn_display(); } ?>
+			<a href="<?php echo get_option('siteurl'); ?>/<?php echo $lang=='pt'?'':$lang; ?>">HOME</a>
+				<?php if(function_exists('bcn_display'))
+				{
+					bcn_display();
+				}?>
 		</div>
 		<h1 class="title">Categoria: <?php single_cat_title(); ?></h1>
 		<?php
