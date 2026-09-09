17a8f4ab24cd4a2d30117d5b29b58de73e3566a0
da Silva <dasilvmar@paho.org>
2026-09-09 13:33:29 -0300

    Ajuste footer DeCS Finder

--- DeCSFinder/footer.php
+++ DeCSFinder/footer.php
@@ -6,7 +6,7 @@
 					<ul type="none"><?php dynamic_sidebar('footer') ?></ul>
 				</div>
 				<div class="col-md-4" id="footer-logo-bir">
-					<a href="https://www.bireme.org/<?php echo $lang === 'pt' ? '' : $lang; ?>" target="_blank"><img src="http://logos.bireme.org/img/<?php echo $lang; ?>/v_bir_white.svg" class="img-fluid" alt=""></a>
+					<a href="https://www.bireme.org/<?php echo $lang === 'pt' ? '' : (in_array($lang, ['en', 'es']) ? $lang : 'en'); ?>" target="_blank"><img src="http://logos.bireme.org/img/<?php echo $lang; ?>/v_bir_white.svg" class="img-fluid" alt=""></a>
 				</div>
 			</div>
 			<hr>
@@ -14,7 +14,7 @@
 	</footer>
 	<div id="powered">
 		<div class="container">
-			<a href="https://www.bireme.org/<?php echo $lang === 'pt' ? '' : $lang; ?>" target="_blank"><img src="<?php bloginfo('template_directory') ;?>/img/powered.svg" alt="BIREME"></a><br>
+			<a href="https://www.bireme.org/<?php echo $lang === 'pt' ? '' : (in_array($lang, ['en', 'es']) ? $lang : 'en'); ?>" target="_blank"><img src="<?php bloginfo('template_directory') ;?>/img/powered.svg" alt="BIREME"></a><br>
 			<small>© <?php pll_e('All rights are reserved'); ?></small> <br>
 		</div>
 	</div>
